b0b781f38221567a3992bdeb1def8d36ad536769
Ngoubou <you@example.com>
2021-12-10 20:58:08 +0100

    Initial commit

---
+++ test.py
@@ -0,0 +1,19 @@
+# 1e étape de l'algorithme marche, on compare 1 nombre sur deux au nombre adjacent
+i = 0
+l = [40, 2, 34, 3, 18, 7] # works only for this example
+
+for i in range(len(l)):
+    
+    while i+1 < len(l):
+        #print(i)
+        temp = l[i]
+        #if i+1 > len(l):
+         #   break
+        if l[i] > l[i+1]:
+            l[i] = l[i+1]
+            l[i+1] = temp
+        i += 2
+print(l)
+
+# the code works for a list of 6 elements (nope)
+# but i'm getting close i feel it
