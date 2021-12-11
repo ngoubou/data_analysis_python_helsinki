99acf141e6b6724849e590fb1e896c1525629a8c
Ngoubou <you@example.com>
2021-12-11 22:06:03 +0100

    Update ex9_merge.py

--- week1/ex9_merge.py
+++ week1/ex9_merge.py
@@ -2,7 +2,8 @@
 #i = 0
 #l = [40, 2, 34, 3, 18, 7] # works only for this example
 l = [1, 5, 9, 12, 2, 6, 10]
-while (l[0] != min(l)) or (l[-1] != max(l)):
+#while (l[0] != min(l)) or (l[-1] != max(l)):
+while(!sorted(l)): # can't use
     for i in range(0, 1 + len(l)):
     
         while i+1 < len(l):
