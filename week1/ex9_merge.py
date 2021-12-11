9a080a1e9e08dc7781424ef4b03b82ed28622c44
Ngoubou <you@example.com>
2021-12-11 23:26:14 +0100

    done

--- week1/ex9_merge.py
+++ week1/ex9_merge.py
@@ -1,9 +1,8 @@
-# 1e étape de l'algorithme marche, on compare 1 nombre sur deux au nombre adjacent
-#i = 0
-#l = [40, 2, 34, 3, 18, 7] # works only for this example
+# test list
 l = [1, 5, 9, 12, 2, 6, 10]
+
 # the loop should continue as long as the list is unsorted
-while( not all(l[j] <= l[j+1] for j in range(len(l)-1))):
+while( not all(l[j] <= l[j+1] for j in range(len(l)-1))): # found on stackoverflow
 #while (l[0] != min(l)) or (l[-1] != max(l)): # should check here if list is sorted
     for i in range(0, 1 + len(l)):
     
@@ -18,5 +17,4 @@ while( not all(l[j] <= l[j+1] for j in range(len(l)-1))):
             i += 2
 print(l)
 
-# the code works for a list of 6 elements (nope)
-# but i'm getting close i feel it 
+# my code wasn't working cause the while condition was not correctly specified
