f97e622510196fec072ec64862a38b5ee4e3975c
Ngoubou <you@example.com>
2021-12-11 21:54:38 +0100

    Update ex9_merge.py

--- week1/ex9_merge.py
+++ week1/ex9_merge.py
@@ -5,17 +5,15 @@ l = [1, 5, 9, 12, 2, 6, 10]
 while (l[0] != min(l)) or (l[-1] != max(l)):
     for i in range(0, 1 + len(l)):
     
-        #while i+1 < len(l):
+        while i+1 < len(l):
             #print(i)
-        temp = l[i]
-        if i+1 > len(l):
-            break
-
-        if l[i] > l[i+1]:
-
-            l[i] = l[i+1]
-            l[i+1] = temp
-        i += 2
+            temp = l[i]
+            #if i+1 > len(l):
+            #   break
+            if l[i] > l[i+1]:
+                l[i] = l[i+1]
+                l[i+1] = temp
+            i += 2
 print(l)
 
 # the code works for a list of 6 elements (nope)
