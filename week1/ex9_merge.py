2e760eec050fe6ba6a644deb47cece2e8cd8fd44
Ngoubou <you@example.com>
2021-12-11 23:23:25 +0100

    Update ex9_merge.py

--- week1/ex9_merge.py
+++ week1/ex9_merge.py
@@ -3,8 +3,8 @@
 #l = [40, 2, 34, 3, 18, 7] # works only for this example
 l = [1, 5, 9, 12, 2, 6, 10]
 # the loop should continue as long as the list is unsorted
-#all(l[i] <= l[i+1] for i in range(len(l)-1))
-while (l[0] != min(l)) or (l[-1] != max(l)): # should check here if list is sorted
+while( not all(l[j] <= l[j+1] for j in range(len(l)-1))):
+#while (l[0] != min(l)) or (l[-1] != max(l)): # should check here if list is sorted
     for i in range(0, 1 + len(l)):
     
         while i+1 < len(l):
