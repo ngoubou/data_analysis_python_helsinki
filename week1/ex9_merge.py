60087ab1fcc1984ba1bf3def4118961cfee08875
Ngoubou <you@example.com>
2021-12-11 21:14:30 +0100

    Update ex9_merge.py

--- week1/ex9_merge.py
+++ week1/ex9_merge.py
@@ -1,7 +1,8 @@
 # 1e étape de l'algorithme marche, on compare 1 nombre sur deux au nombre adjacent
 i = 0
-l = [40, 2, 34, 3, 18, 7] # works only for this example
-
+#l = [40, 2, 34, 3, 18, 7] # works only for this example
+l = [89,56,98,32]
+while (l[0] != min(l)) and (l[-1] != max(l)):
 for i in range(len(l)):
     
     while i+1 < len(l):
