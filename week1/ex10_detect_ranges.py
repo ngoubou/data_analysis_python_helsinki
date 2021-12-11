9c441da5735af533849c393e5e46d38b57d562be
Ngoubou <you@example.com>
2021-12-11 23:28:02 +0100

    exo10

---
+++ week1/ex10_detect_ranges.py
@@ -0,0 +1,20 @@
+# test list
+l = [1, 5, 9, 12, 2, 6, 10]
+
+# the loop should continue as long as the list is unsorted
+while( not all(l[j] <= l[j+1] for j in range(len(l)-1))): # found on stackoverflow
+#while (l[0] != min(l)) or (l[-1] != max(l)): # should check here if list is sorted
+    for i in range(0, 1 + len(l)):
+    
+        while i+1 < len(l):
+            #print(i)
+            temp = l[i]
+            #if i+1 > len(l):
+            #   break
+            if l[i] > l[i+1]:
+                l[i] = l[i+1]
+                l[i+1] = temp
+            i += 2
+print(l)
+
+# my code wasn't working cause the while condition was not correctly specified
