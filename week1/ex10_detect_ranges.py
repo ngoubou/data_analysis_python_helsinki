bf272d9611edfd214d74d1e7a17254ea04d33e08
Ngoubou <you@example.com>
2021-12-13 21:13:30 +0100

    Update ex10_detect_ranges.py

--- week1/ex10_detect_ranges.py
+++ week1/ex10_detect_ranges.py
@@ -1,6 +1,13 @@
+# coding=utf-8
 # write a function that detect ranges between numbers
 # i think i've understood what is asked (after many hours)
 # but don't know how to explain it in plain english
+
+# Create a function named detect_ranges that gets a list of integers as a parameter. 
+# The function should then sort this list, and transform the list into another list 
+# where pairs are used for all the detected intervals. So 3,4,5,6 is replaced by the pair (3,7). 
+# Numbers that are not part of any interval result just single numbers. 
+# The resulting list consists of these numbers and pairs, separated by commas.
 
 # what i have
 l = [2,5,4,8,12,6,7,10,13]
@@ -11,8 +18,15 @@ l = [2,5,4,8,12,6,7,10,13]
 ls = [2, 4, 5, 6, 7, 8, 10, 12, 13]
 
 
-for i in range(4,7):
-    print(i)
+for i in range(len(ls)):
+    # écrire une condition pour ne pas prendre dernier élément
+    # car i + 1 out of range
+    if len(range(ls[i], ls[i+1])) == 2:
+        "oui"
+    print(range(ls[i], ls[i+1]))
+    #print(i)
+
+
 
 # prends 2 nombres adjacents (i et i + 1)
 # calcule range(i, i+1)
