6c9db7f80efbe5b08757a0ea41b09a27e6e5d3cc
Ngoubou <you@example.com>
2021-12-15 12:48:17 +0100

    pseudocode

--- week1/test.py
+++ week1/test.py
@@ -1,14 +1,23 @@
 # coding=utf-8
 
-ls = [2, 4, 3,5, 6, 7, 8, 10, 12, 13]
+ls = [2, 4, 5, 6, 7, 8, 10, 12, 13]
 ll = ls.copy()
 del ll[0]
 
 
 # faire range des 2 1ers éléments et les mettre dans une liste
-# vérifier si les éléments de cette liste dans notre LA LISTE
-# s'ils le sont tous étendre le range s'arrêter dès qu'il y a un élément non présent dans THE LIST
-# s'il y a un élément
+list
+# vérifier si tous les éléments de cette liste sont dans LA LISTE:
+    # s'ils le sont tous, étendre le range à l'élément suivant.
+    # s'arrêter dès qu'il y a un élément non présent dans THE LIST
+    # et print range(élément[0], élément[-1])
+    # prends 2e nombre du range
+    # si 2e nombre pas dans LISTE, alors dernier élément à imprimer
+
+    # s'il y a un élément non présent dans LA LISTE:
+    # print le 1er nombre, et sors
+    # update une variable ici
+    # prends le 2e nombre avec qui tu étais et fais ce qui est au dessus
 
 
 
