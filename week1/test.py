5664b6a089b782ee5a4434ccb28b2de9e551c1e5
Ngoubou <you@example.com>
2021-12-15 12:03:48 +0100

    getting closer

--- week1/test.py
+++ week1/test.py
@@ -3,13 +3,20 @@ ll = ls.copy()
 del ll[0]
 
 for j in ll:
-    for i in range(len(ls)):
+    for i in ls:
         # s'il y a un élément du résultat qui n'est pas dans la liste imprime i
         for k in list(range(i,j)):
-            if k == ls[i]:
+            if k in ls: # dois vérifier ici que k est dans ls
                 print(k)
+            else:
+                print(j)
         #if j not in list(range(i,j)):
         #if (list(range(i,j))) and (j not in list(range(i,j))):
          #   print(i)
         #print(list(range(i,j)))
         #print(range(j,i))
+
+# je dois garder les deux loupes suivantes
+# for j in ll:
+    # for i in ls:
+# peut être en modifier l'ordre
