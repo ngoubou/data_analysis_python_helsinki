8d2131fe7574fd82c47abc16b61b005f22ab9f40
Ngoubou <you@example.com>
2021-12-15 14:13:42 +0100

    .

--- week1/test.py
+++ week1/test.py
@@ -8,18 +8,19 @@ del ll[0]
 
 # faire range des 2 1ers éléments et les mettre dans une liste
 for i in range(len(ls)): 
+    while i+1 < len(ls):
 # for i in ls:
     #new_list =  list(range(i, i+1)) # donne un résultat différent
     
-    new_list = list(range(ls[i], ls[i+1])) # attention au dernier élément de la liste
+        new_list = list(range(ls[i], ls[i+1])) # attention au dernier élément de la liste
 
 
 
 # vérifier si tous les éléments de cette liste sont dans LA LISTE:
 #print(all(x in ls for x in new_list))
     # s'ils le sont tous, étendre le range à l'élément suivant.
-    if all(x in ls for x in new_list):
-        print(range(ls[i], ls[i+1]))
+        if all(x in ls for x in new_list):
+            print(range(ls[i], ls[i+1]))
 
     # s'arrêter dès qu'il y a un élément non présent dans THE LIST
     # et print range(élément[0], élément[-1])
@@ -27,8 +28,9 @@ for i in range(len(ls)):
     # si 2e nombre pas dans LISTE, alors dernier élément à imprimer
 
     # s'il y a un élément non présent dans LA LISTE:
-    else:
-        print(ls[i])
+        else:
+            print(ls[i])
+        i += 1
     
     # print le 1er nombre, et sors
     # update une variable ici
