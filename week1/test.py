eefcb1021386746e44bad43ee3f9924a5be16fb7
Ngoubou <you@example.com>
2021-12-15 11:53:39 +0100

    la hess

--- week1/test.py
+++ week1/test.py
@@ -3,10 +3,13 @@ ll = ls.copy()
 del ll[0]
 
 for j in ll:
-    for i in ls:
+    for i in range(len(ls)):
         # s'il y a un élément du résultat qui n'est pas dans la liste imprime i
-        if j not in list(range(i,j)):
+        for k in list(range(i,j)):
+            if k == ls[i]:
+                print(k)
+        #if j not in list(range(i,j)):
         #if (list(range(i,j))) and (j not in list(range(i,j))):
-            print(i)
+         #   print(i)
         #print(list(range(i,j)))
         #print(range(j,i))
