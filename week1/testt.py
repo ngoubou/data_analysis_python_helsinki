5dd23a5f4fd4c3a66903f9147bb8706415a09840
Ngoubou <you@example.com>
2021-12-16 22:40:40 +0100

    works for one example

--- week1/testt.py
+++ week1/testt.py
@@ -1,7 +1,7 @@
 # coding=utf-8
 
-ls = [88, 89, 90, 92, 93, 94, 95, 96, 97]
-
+#ls = [88, 89, 90, 92, 93, 94, 95, 96, 97]
+ls = [2, 5, 4, 8, 12, 6, 7, 10, 13]
 #j = 1
 #for i in range(len(ls)):
    # new_list = list(range(ls[i], ls[j])) 
@@ -10,24 +10,25 @@ new_list = [ls[0], ls[1]]
 j = 1
 i = 0
 # y aura une loop ici pour revenir checker (doit commencer à 0)
-while True: # évidemment mettre autre condition comme i < len(ls)
+while j + 1 < len(ls): # évidemment mettre autre condition comme i < len(ls) ou plutôt j
 # 1e loop pour le cas où tous les éléments de new_list(range) sont dans ls
     while (all(x in ls for x in new_list)) and j + 1 < len(ls): 
         #i = 0 # ne doit pas être ici
-        print(range(ls[i], ls[j]))
+        #print(range(ls[i], ls[j]))
         new_list = list(range(ls[i], ls[j+1]))
         j += 1 
 
         if ls[-1] == new_list[-1] + 1:
             print(range(new_list[0], ls[-1] + 1))
+            #break
             
-    #print(new_list) # ou range(new_list)
-    print(range(new_list))
-
+    #print(new_list) # voir combien de fois et quand l'imprimer
+    if j + 1 < len(ls): # ls[0] == new_list[0] ?
+        print(range(new_list[0], new_list[-1]))
 # 2e loop pour l'autre cas
     i = j
     while not all(x in ls for x in new_list):
     
-        print(range(ls[i], ls[j+1]))
+        #print(range(ls[i], ls[j+1]))
         new_list = list(range(ls[i], ls[j+1]))
         j += 1
