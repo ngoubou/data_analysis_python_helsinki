9e4c2a2c1c8f2a9cae311d053cd2a62642f5c264
Ngoubou <you@example.com>
2021-12-17 16:00:15 +0100

    works for both test cases; formatting to solve

---
+++ week1/test1.py
@@ -0,0 +1,30 @@
+# coding=utf-8
+
+ls = [88, 89, 90, 92, 93, 94, 95, 96, 97]
+#ls = sorted([2, 5, 4, 8, 12, 6, 7, 10, 13])
+#ls = [2, 4, 5, 6, 7, 8, 10, 12, 13]
+new_list = list(range(ls[0], ls[1]))
+j = 1
+#for i in range(len(ls)):
+while j < len(ls):
+    #for j in range(1,len(ls)): # mettre while loop ici
+    while (not all(x in ls for x in new_list)) and (j < len(ls)): #  mettre condition pour dernier élément
+        if len(new_list) == 2:
+            print(new_list[0]) # créer une liste pour toutes les valeurs
+                #new_list = list(range(ls[i+1], ls[1]))
+            new_list = list(range(ls[j], ls[j+1])) # mettre autre chose ici
+            j += 1
+        else:
+            print(new_list[0], new_list[-1]) # créer une liste pour toutes les valeurs
+            new_list = list(range(ls[j-1], ls[j]))
+        
+    while (all(x in ls for x in new_list)) and (j < len(ls)): # mettre condition pour dernier élément
+        new_list = list(range(new_list[0], ls[j]))
+        j += 1
+            # pour dernier élément
+        if ls[-1] == new_list[-1] + 1:
+            print(new_list[0], ls[-1] + 1) # créer une liste pour toutes les valeurs
+                #print(range(new_list[0], ls[-1] + 1)) 
+
+    #while all(x in ls for x in new_list):
+        # 1+1
