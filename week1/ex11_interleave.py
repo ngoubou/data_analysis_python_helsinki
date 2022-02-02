8a538ea59f871bf6209a55e5dfb9cdba99cb1526
Ngoubou <you@example.com>
2022-02-02 17:28:03 +0100

    start exo 11

---
+++ week1/ex11_interleave.py
@@ -0,0 +1,7 @@
+# Write function interleave that gets arbitrary number of lists as parameters. 
+# You may assume that all the lists have equal length. 
+# The function should return one list containing all the elements from the input lists interleaved. 
+# Test your function from the main function of the program.
+
+# Example: interleave([1,2,3], [20,30,40], ['a', 'b', 'c']) should return [1, 20, 'a', 2, 30, 'b', 3, 40, 'c'].
+#  Use the zip function to implement interleave. Remember the extend method of list objects.
