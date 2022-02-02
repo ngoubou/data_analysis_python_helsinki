9596cb7363509f4593be1318f55758d137d71088
Ngoubou <you@example.com>
2022-02-02 17:46:08 +0100

    done ex11 faster than steph currry shot

--- week1/ex11_interleave.py
+++ week1/ex11_interleave.py
@@ -5,3 +5,20 @@
 
 # Example: interleave([1,2,3], [20,30,40], ['a', 'b', 'c']) should return [1, 20, 'a', 2, 30, 'b', 3, 40, 'c'].
 #  Use the zip function to implement interleave. Remember the extend method of list objects.
+
+def interleave(*lists):
+    ls = []
+    for i in zip(*lists):
+        ls.extend(i)
+
+        #print(list(i))
+    #return list(zip(*lists))
+    return ls
+
+def main():
+    print(interleave([1, 2, 3], [20, 30, 40], ['a', 'b', 'c']))
+
+if __name__ == "__main__":
+    main()
+
+# easiest exercise of the whole course so far. Took me less than 30 minutes
