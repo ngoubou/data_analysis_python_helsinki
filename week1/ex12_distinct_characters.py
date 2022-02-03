8998f7927aa4b7d747bf9394f7331474725fb752
Ngoubou <you@example.com>
2022-02-03 10:52:54 +0100

    count unique character of each element

--- week1/ex12_distinct_characters.py
+++ week1/ex12_distinct_characters.py
@@ -4,7 +4,26 @@ def distinct_characters(L):
     return {}
 
 def main():
-    print(distinct_characters(["check", "look", "try", "pop"]))
+    ls = ["check", "look", "try", "pop"]
+    print(distinct_characters(ls))
 
 if __name__ == "__main__":
     main()
+
+# Write function distinct_characters that gets a list of strings as a parameter.
+# It should return a dictionary whose keys are the strings of the input list 
+# and the corresponding values are the numbers of distinct characters in the key.
+
+# Use the set container to temporarily store the distinct characters in a string. 
+# Example of usage: distinct_characters(["check", "look", "try", "pop"]) should return
+#  { "check" : 4, "look" : 3, "try" : 3, "pop" : 2}.
+
+s = set(["check", "look", "try", "pop"])
+for i, x in enumerate(s):
+    #print(i)
+    print(x)
+    print(len(set(x)))
+
+# écrire for loop combiné à enumerate
+# afin d'accéder à chaque élément du set
+# puis compter longeur
