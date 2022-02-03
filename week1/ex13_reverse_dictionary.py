600123a74acdd50b7a0f97ae676bbf5655a26b00
Ngoubou <you@example.com>
2022-02-03 15:12:13 +0100

    extract keys and values of the dic

--- week1/ex13_reverse_dictionary.py
+++ week1/ex13_reverse_dictionary.py
@@ -1,28 +1,24 @@
 #!/usr/bin/env python3
 
-def reverse_dictionary(d):
-    return {}
-
-def main():
-    pass
-
-if __name__ == "__main__":
-    main()
-
-# Let d be a dictionary that has English words as keys and a list of Finnish words as values. 
-# So, the dictionary can be used to find out the Finnish equivalents of an English word in the following way:
-d = {}
-d["move"]
-["liikuttaa"]
-d["hide"]
-["piilottaa", "salata"]
-
 # Make a function reverse_dictionary that creates a Finnish to English dictionary based on a English 
 # to Finnish dictionary given as a parameter. 
 # The values of the created dictionary should be lists of words. It should work like this:
 
 d={'move': ['liikuttaa'], 'hide': ['piilottaa', 'salata'], 'six': ['kuusi'], 'fir': ['kuusi']}
-reverse_dictionary(d)
-{'liikuttaa': ['move'], 'piilottaa': ['hide'], 'salata': ['hide'], 'kuusi': ['six', 'fir']}
+#reverse_dictionary(d)
+#{'liikuttaa': ['move'], 'piilottaa': ['hide'], 'salata': ['hide'], 'kuusi': ['six', 'fir']}
 
 # Be careful with synonyms and homonyms!
+
+### my solution ----
+
+# try unpacking the items in d into variables
+s = {"me":1, "no":2}
+
+# get dic keys
+x = set(s.keys())
+# get dic values
+y = set(s.values())
+
+print(x,y)
+
