ca176386097c7c6c97986e67e6c5e20249f870f4
Ngoubou <you@example.com>
2022-02-03 13:50:30 +0100

    update instructions

--- week1/ex13_reverse_dictionary.py
+++ week1/ex13_reverse_dictionary.py
@@ -9,3 +9,20 @@ def main():
 if __name__ == "__main__":
     main()
 
+# Let d be a dictionary that has English words as keys and a list of Finnish words as values. 
+# So, the dictionary can be used to find out the Finnish equivalents of an English word in the following way:
+d = {}
+d["move"]
+["liikuttaa"]
+d["hide"]
+["piilottaa", "salata"]
+
+# Make a function reverse_dictionary that creates a Finnish to English dictionary based on a English 
+# to Finnish dictionary given as a parameter. 
+# The values of the created dictionary should be lists of words. It should work like this:
+
+d={'move': ['liikuttaa'], 'hide': ['piilottaa', 'salata'], 'six': ['kuusi'], 'fir': ['kuusi']}
+reverse_dictionary(d)
+{'liikuttaa': ['move'], 'piilottaa': ['hide'], 'salata': ['hide'], 'kuusi': ['six', 'fir']}
+
+# Be careful with synonyms and homonyms!
