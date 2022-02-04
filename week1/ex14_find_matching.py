e81046a332590a3841f39e5c752c155d81bf9c53
Ngoubou <you@example.com>
2022-02-04 13:25:06 +0100

    done

--- week1/ex14_find_matching.py
+++ week1/ex14_find_matching.py
@@ -1,10 +1,19 @@
 #!/usr/bin/env python3
 
 def find_matching(L, pattern):
-    return []
+    ls = []
+    for i, x in enumerate(L):
+        if pattern in x:
+            ls.append(i)
+    return ls
 
 def main():
     pass
 
 if __name__ == "__main__":
     main()
+
+
+## COURSE SOLUTION ----
+
+# same as mine
