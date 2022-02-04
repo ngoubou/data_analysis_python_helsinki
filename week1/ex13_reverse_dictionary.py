8703092c6ad564df3a6935ebd65e2abee62f5787
Ngoubou <you@example.com>
2022-02-04 12:16:14 +0100

    done and added course solution

--- week1/ex13_reverse_dictionary.py
+++ week1/ex13_reverse_dictionary.py
@@ -68,3 +68,48 @@ print(a)
 #print(x,y)
 
 
+## COURSE SOLUTION ----
+
+def reverse_dictionary(d):
+    
+    result = {}
+
+    for key, value in d.items():
+
+        for v in value:
+
+            if v in result:
+
+                result[v].append(key)
+
+            else:
+
+                result[v] = [key]
+
+    return result
+
+ 
+
+def main():
+
+    d = {"move":["liikuttaa"], "hide":["piilottaa", "salata"]}
+
+    print(d)
+
+    print(reverse_dictionary(d))
+
+ 
+
+    d = {"move":["liikuttaa"], "hide":["piilottaa", "salata"],
+
+         "six" : ["kuusi"], "fir" : ["kuusi"]}
+
+    print(d)
+
+    print(reverse_dictionary(d))
+
+ 
+
+if __name__ == "__main__":
+
+    main()
