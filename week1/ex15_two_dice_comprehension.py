727c41d0b8e1027c8ccd2388a173d60d4a0de138
Ngoubou <you@example.com>
2022-02-04 13:28:56 +0100

    start exercise 15

---
+++ week1/ex15_two_dice_comprehension.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+# Redo the earlier exercise which printed all the pairs of two dice results that sum to 5. 
+# But this time use a list comprehension. Print one pair per line.
