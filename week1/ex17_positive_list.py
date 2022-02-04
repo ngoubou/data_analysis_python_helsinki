86c2eb5547503ee562007cf2927c719c1b53dc7d
Ngoubou <you@example.com>
2022-02-04 15:21:59 +0100

    done

--- week1/ex17_positive_list.py
+++ week1/ex17_positive_list.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def positive_list(L):
-    return []
+    def is_positive(x):
+        """Returns True if x is posiive and False if not"""
+        return x > 0
+    
+    return list(filter(is_positive, L))
 
 def main():
     pass
@@ -9,8 +13,8 @@ def main():
 if __name__ == "__main__":
     main()
 
-# Write a function positive_list that gets a list of numbers as a parameter, 
-# and returns a list with the negative numbers and zero filtered out using the filter function.
+## COURSE SOLUTION ----
 
-# The function call positive_list([2,-2,0,1,-7]) should return the list [2,1].
-#  Test your function in the main function.
+#def positive_list(L):
+    
+#    return list(filter(lambda x: x > 0, L))
