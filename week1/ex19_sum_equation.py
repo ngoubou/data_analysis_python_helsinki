c08d82a441beb035b62545f3c0e87348608c5e65
Ngoubou <you@example.com>
2022-02-05 12:25:58 +0100

    start ex19

---
+++ week1/ex19_sum_equation.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+def sum_equation(L):
+    return ""
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
+# Write a function sum_equation which takes a list of positive integers as parameters and returns a string
+# with an equation of the sum of the elements.
+
+# Example: sum_equation([1,5,7]) returns "1 + 5 + 7 = 13" Observe, the spaces should be exactly as shown above.
+# For an empty list the function should return the string "0 = 0".
