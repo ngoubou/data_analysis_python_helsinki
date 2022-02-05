365881dcbef4cc408e34c0edd68ecc0bcd0bd9f4
Ngoubou <you@example.com>
2022-02-05 14:27:13 +0100

    done

--- week1/ex19_sum_equation.py
+++ week1/ex19_sum_equation.py
@@ -1,7 +1,28 @@
 #!/usr/bin/env python3
 
 def sum_equation(L):
-    return ""
+    # L = [1, 5, 7]
+    # 1 - écrire un programme qui additionne tous les éléments de la liste, convertir résultat en string"
+    
+    from functools import reduce   # import the reduce function from the functools module
+    add = reduce(lambda x,y:x+y, L, 0)
+    add = str(add)
+
+    # 2 - convertir la liste en string (ou boxer mouhahahaha)
+    #s1 = str([1,5,7]) # not correct
+    # i want to convert each element of the list as a string, so i use the following method
+    s1 = [str(x) for x in L]
+
+    # 3 - ajouter signe "+" à la liste convertie et signe "="
+    s2 = " + ".join(s1)
+
+    # 4 - concatener liste au résultat de l'addition
+    if len(L) > 0:
+        a = s2 + " = " + add
+    else: # handle empty lists
+        a = "0 = 0"
+
+    return a
 
 def main():
     pass
@@ -9,42 +30,16 @@ def main():
 if __name__ == "__main__":
     main()
 
-# Write a function sum_equation which takes a list of positive integers as parameters and returns a string
-# with an equation of the sum of the elements.
-
-# Example: sum_equation([1,5,7]) returns "1 + 5 + 7 = 13" Observe, the spaces should be exactly as shown above.
-# For an empty list the function should return the string "0 = 0".
 
 
+## Course Solution ----
 
+#def sum_equation(L):
+    
+    #if not L:
 
-#print(list(map(lambda x: x+x, s1)))
+     #   return "0 = 0"
 
-# 1 - écrire un programme qui additionne tous les éléments de la liste, convertir résultat en string"
-s = [1,5,7]
-from functools import reduce   # import the reduce function from the functools module
-add = reduce(lambda x,y:x+y, s, 0)
+    #result = list(map(str, L))
 
-add = str(add)
-
-# 2 - convertir la liste en string (ou boxer mouhahahaha)
-#s1 = str([1,5,7]) # not correct
-# i want to convert each element of the list as a string, so i use the following method
-s1 = [str(x) for x in s]
-
-
-# 3 - ajouter signe "+" à la liste convertie et signe "="
-s2 = " + ".join(s1)
-print(s2)
-
-print(s2 + " = " + add)
-
-# 4 - concatener liste au résultat de l'addition
-
-
-# print(str(s))
-# print(" ".join(s))
-#a = " + ".join(s1)
-# print(len(a))
-# print(a.strip("+"))
-# print(a)
+    #return " + ".join(result) + f" = {sum(L)}"
