7747779ce9f45aa6e6a57880e36164c4a48f3f16
Ngoubou <you@example.com>
2022-02-05 13:58:40 +0100

    sum all  elements of input list

--- week1/ex19_sum_equation.py
+++ week1/ex19_sum_equation.py
@@ -14,3 +14,24 @@ if __name__ == "__main__":
 
 # Example: sum_equation([1,5,7]) returns "1 + 5 + 7 = 13" Observe, the spaces should be exactly as shown above.
 # For an empty list the function should return the string "0 = 0".
+
+s = str([1,5,7])
+s1 = [1,5,7]
+#print(list(map(lambda x: x+x, s1)))
+
+# 1 - écrire un programme qui additionne tous les éléments de la liste, convertir résultat en string"
+
+from functools import reduce   # import the reduce function from the functools module
+print(reduce(lambda x,y:x+y, s1, 0))
+
+# 2 - convertir la liste en string (ou boxer mouhahahaha)
+# 3 - ajouter signe "+" à la liste convertie et signe "="
+# 4 - concatener liste au résultat de l'addition
+
+
+# print(str(s))
+# print(" ".join(s))
+a = " + ".join(s)
+# print(len(a))
+# print(a.strip("+"))
+# print(a)
