8f0e073660d974f4e2df07f6e16406c7a189e0bb
Ngoubou <you@example.com>
2022-02-05 15:33:09 +0100

    start ex20

---
+++ week1/ex20_usemodule.py
@@ -0,0 +1,11 @@
+# Create your own module as file triangle.py in the src folder. The module should contain two functions:
+
+    # hypothenuse which returns the length of the hypothenuse when given the lengths 
+    # of two other sides of a right-angled triangle
+
+    # area which returns the area of the right-angled triangle, when two sides, 
+    # perpendicular to each other, are given as parameters.
+
+# Make sure both the functions and the module have descriptive docstrings. 
+# Add also the __version__ and __author__ attributes to the module.
+# Call both your functions from the main function (which is in file usemodule.py).
