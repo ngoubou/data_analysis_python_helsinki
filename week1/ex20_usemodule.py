affca5bb6cc6ab11cf6e2dc4599f86aea16c4a21
Ngoubou <you@example.com>
2022-02-05 16:38:50 +0100

    done week 1

--- week1/ex20_usemodule.py
+++ week1/ex20_usemodule.py
@@ -19,16 +19,5 @@ def area(b, h):
 
 __author__ = "Lionel Ngoubou"
 __version__ = "0.1"
-# __doc__ = "i do not exactly know what to write here, just hope it'll solve my problem"
 
-# Create your own module as file triangle.py in the src folder. The module should contain two functions:
-
-    # hypothenuse which returns the length of the hypothenuse when given the lengths 
-    # of two other sides of a right-angled triangle
-
-    # area which returns the area of the right-angled triangle, when two sides, 
-    # perpendicular to each other, are given as parameters.
-
-# Make sure both the functions and the module have descriptive docstrings. 
-# Add also the __version__ and __author__ attributes to the module.
-# Call both your functions from the main function (which is in file usemodule.py).
+# Done Week 1 finally !!!!
