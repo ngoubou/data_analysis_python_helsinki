2a09c59fef3417a98ae00bc84e3e7e2757a90adf
Ngoubou <you@example.com>
2022-02-08 17:23:53 +0100

    start ex1

---
+++ week2/ex1_integers_in_brackets.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+
+
+def integers_in_brackets(s):
+    return []
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
+# Write function integers_in_brackets that finds from a given string all integers
+# that are enclosed in brackets.
+
+# Example run: integers_in_brackets(" afd [asd] [12 ] [a34] [ -43 ]tt [+12]xxx") returns [12, -43, 12]. 
+# So there can be whitespace between the number and the brackets, 
+# but no other character besides those that make up the integer.
+
+# Test your function from the main function.
