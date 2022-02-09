1e43a8b8bc4b7fb88960a773e22eb3405e424411
Ngoubou <you@example.com>
2022-02-09 19:15:20 +0100

    using regex101 website to see what mine is doing

--- week2/ex1_integers_in_brackets.py
+++ week2/ex1_integers_in_brackets.py
@@ -28,43 +28,20 @@ import re
 s = "  afd [128+] [47 ] [a34]  [ +-43 ]tt [+12]xxx"
 #output = re.findall(r'[+-]?\d+', s) #[12, -43, 12]
 
-# i want everything that starts with either a digit or [+-]
+# i want everything that starts with either a digit, or [+-] but not both [+-]
 # and ends with a digit
 
 
 # use map to convert each element of the list as an integer
 # before printing it
 
-#print(output)
-#-\b\d+\b #|\b[\+-]
+
 
 # numbers starting and ending with a digit
-#print(re.findall(r'\b\d+\b', s)) # best so far
-#print(re.findall(r'[^a-zA-Z_+]?\d+', s))
-#print(re.findall(r'[^\-]?\d+', s)) # pas bon
-output = re.findall(r'[-]?\b\d+\b', s)
-#print(output)
+
+output = re.findall(r'[-]?\b[\d]\d\b', s)
+#print(re.findall(r'\b\d?\d\b', s))
+print(output)
 #print(list(map(int, output))) # got it
-#print(re.findall(r'[+-]?\d+', s))
-print(re.findall(r'[-]?\b[\d]\d\b', s)) # PARTIR DE LUI
-#print(re.findall(r'^[^+-]', s))
-#print(re.findall(r'[^+\d]+', s))
-#print(re.findall(r'[+-]?\d+\b', s))
-
-# print everything that ends with a digit
-#print(re.findall(r'\d+\b', s)) # prints every end digit if i dont use '+'
-
-# print everything that starts with a digit
-#print(re.findall(r'\b\d+', s))
-
-# print everything that starts with "+"
-#print(re.findall(r'\b\++', s))
-
-#print(re.findall(r'[+-]?\d+', s))
-print(re.search(r'\d+ (\d+) \d+ (\d+)', 'first 123 45 67 890 last'))
-#a = "+-43 25 48"
-#print(re.findall(r'^(-|\+)', a))
-#print(re.findall(r'[^\d\b]+\b\d+\b', a)) # prends +-43
-#print(re.findall(r'[^\d\b]+', a)) # prends +- et 'top'
-#print(re.findall(r'[^ea]', a))
-#print(re.findall(r'^"ea"', a))
+# [^\[\]+?=\-] # matches everything but "+" and "-"
+# '[^\+\-][-]?\b[\d]\d\b' # matches 47
