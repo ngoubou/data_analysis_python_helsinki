96c6dfe6b3c3e973b6c801e370da9d6143c89755
Ngoubou <ralphngoubou@gmail.com>
2022-02-21 10:31:37 +0100

    done

--- week2/ex5_summary.py
+++ week2/ex5_summary.py
@@ -8,43 +8,58 @@ from math import sqrt
 
 def summary(filename):
     
-    if filename:
 
-        with open(filename, "r") as f:
-            ls = []
-            add = []
-            moyenne = []
-            sd = []
-            for line in f:
-                try:
-                    line = float(line.strip("\n"))           # The float constructor raises ValueError exception if conversion is no possible
-                except ValueError:
-                    continue
+    with open(filename, "r") as f:
+        ls = []
+        add = []
+        moyenne = []
+        sd = []
 
-                ls.append(line)
+        for line in f:
+            try:
+                line = float(line.strip("\n"))           # The float constructor raises ValueError exception if conversion is no possible
+            except ValueError:
+                continue
+            ls.append(line)
     
-        add.append(sum(ls))
-        moyenne.append(mean(ls))
-        sd.append(sqrt(variance(ls)))
-    
-    else: # what to do if empty file
-        add = [float(0)]
-        moyenne = [float(0)]
-        sd = [float(0)]
+    add.append(sum(ls))
+    moyenne.append(mean(ls))
+    sd.append(sqrt(variance(ls)))
 
-    return (float(add[0]), float(moyenne[0]), float(round(sd[0], 6)))
+    return (float(add[0]), float(moyenne[0]), float(sd[0]))
 
 def main():
-   
-    for i in sys.argv[1:]: 
-        print(summary(sys.argv[1:]))
-        
-        print(f"File: {i} Sum: {51.4:.6f} Average: {10.28:.6f} Stdev: {8.904606:.6f}")
-        print(f"File: {i} Sum: {5446.2:.6f} Average: {1815.4:.6f} Stdev: {3124.294045:.6f}")
-        print(f"File: {i} Sum: {0:.6f} Average: {0:.6f} Stdev: {50:.6f}")
-        print(f"File: {i} Sum: {0:.6f} Average: {0:.6f} Stdev: {0:.6f}")
-        print(f"File: {i} Sum: {0:.6f} Average: {0:.6f} Stdev: {0:.6f}")
-        print(f"File: {i} Sum: {0:.6f} Average: {0:.6f} Stdev: {0:.6f}")
+    
+    for i in sys.argv[1:]:
+        z = summary(i)
+        print(f"File: {i} Sum: {z[0]:.6f} Average: {z[1]:.6f} Stddev: {z[2]:.6f}")
 
 if __name__ == "__main__":
     main()
+
+## Course Solution ----
+
+#from statistics import stdev
+
+#import sys
+
+#def summary(filename):
+#    L=[]
+#    with open(filename) as f:
+#        for line in f:
+#            try:
+                #L.append(float(line))
+#            except ValueError:
+#                continue
+#    s = sum(L)
+#    a = s/len(L)
+#    stddev = stdev(L)
+
+#    return s, a, stddev
+
+ 
+
+#def main():
+#    for filename in sys.argv[1:]:
+#        s, a, stddev = summary(filename)
+#        print(f"File: {filename} Sum: {s:.6f} Average: {a:.6f} Stddev: {stddev:.6f}")
