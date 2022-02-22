1cd3f565db976c2b6723e76f30aab66a2cc5fe72
Ngoubou <ralphngoubou@gmail.com>
2022-02-22 15:36:13 +0100

    done

--- week2/ex8_prepend.py
+++ week2/ex8_prepend.py
@@ -1,38 +1,22 @@
 #!/usr/bin/env python3
 
 class Prepend(object):
-    # Add the methods of the class here
     """Create a class called Prepend"""
     
-    def __init__(self, param1):#, param2):
+    def __init__(self, start):
         "This initialises an instance of type ClassName"
-        self.start = param1 # creates an instance attribute
-        #c = param2      # creates a local variable of the function
-        # statements ...
+        self.start = start # creates an instance attribute
     
-    def write(self):#, param1):
+    def write(self, s):#, param1):
         """This is a method of the class"""
-        # some statements
-        print(self)
-    
-    a = 1 # This creates a class attribute
+        print(self.start + s)
+ 
 
 def main():
-    pass
+    p = Prepend("+++ ")    
+    p.write("Hello")
 
 if __name__ == "__main__":
     main()
 
-# Create a class called Prepend. We create an instance of the class by giving a string as a parameter
-# to the initializer. The initializer stores the parameter in an instance attribute start. 
-# The class also has a method write(s) which prints the string s prepended with the start string. 
-# An example of usage:
-
-# p = Prepend("+++ ")
-#p.write("Hello");
-
-# Will print
-
-# +++ Hello
-
-# Try out using the class from the main function.
+# One of the rare instances where my solution equals the course one
