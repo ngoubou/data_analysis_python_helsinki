542c6604c5db1891b55cc798eea50f9f5309a29f
Ngoubou <ralphngoubou@gmail.com>
2022-02-22 13:23:05 +0100

    start ex8

---
+++ week2/ex8_prepend.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+class Prepend(object):
+    # Add the methods of the class here
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
+# Create a class called Prepend. We create an instance of the class by giving a string as a parameter
+# to the initializer. The initializer stores the parameter in an instance attribute start. 
+# The class also has a method write(s) which prints the string s prepended with the start string. 
+# An example of usage:
+
+# p = Prepend("+++ ")
+#p.write("Hello");
+
+# Will print
+
+# +++ Hello
+
+# Try out using the class from the main function.
