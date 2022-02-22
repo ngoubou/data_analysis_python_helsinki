d423517c7d4e4e2df9a49afd576422a63d15dd23
Ngoubou <ralphngoubou@gmail.com>
2022-02-22 17:24:19 +0100

    class inheritance

--- week2/ex9_rational.py
+++ week2/ex9_rational.py
@@ -2,19 +2,34 @@
 
 class Rational(object):
     """Create a class called Rational"""
+    #x = 0
     def __init__(self, num1, num2):
         "This initialises an instance of type ClassName"
         self.num1 = num1 # creates an instance attribute
         self.num2 = num2
         #c = num1 / num2
     def __truediv__(self):
+        #x += 1
         print(self.num1/self.num2)
+    #print(__truediv__())
+    #print(x)
+#class Test(Rational):
+ #   def __init__(self, num1, num2):
+  #      self.num1 = num1 # creates an instance attribute
+   #     self.num2 = num2
+    #print(Rational.__truediv__())
+    #pass
+
+## USE CLASS INHERITANCE 
 
 def main():
     r1=Rational(1,4)
     r2=Rational(2,3)
-    print(r1.__truediv__())
-    print(r2)
+    print(r1)
+    #r3 = Test(1,4)
+    #print(r3)
+    #print(r1.__truediv__()) #the result i want
+    #print(r2)
    # print(r1*r2)
     #print(r1/r2)
     #print(r1+r2)
