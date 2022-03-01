a763ac80cad6f5d07c7123393f22f2e040fae2d2
Ngoubou <ralphngoubou@gmail.com>
2022-03-01 15:44:32 +0100

    implemented multiplication

--- week2/ex9_rational.py
+++ week2/ex9_rational.py
@@ -7,15 +7,18 @@ class Rational:
         self.first = first
         self.last = last
 
-    def __repr__(self):
+    def __repr__(self): # support printing floats
         return f"{self.first / self.last}"
     
-
+    # i now implement the basic operations and comparisons
+    def __mul__(self, s):
+        return Rational(self.first * s.first, self.last * s.last)
 def main():
     r1 = Rational(1,4)
     r2 = Rational(2, 3)
     print(r1)
     print(r2)
+    print(r1*r2)
 
 
 if __name__ == "__main__":
