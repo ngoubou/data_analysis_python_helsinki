685a004350d5b088bb27e215c847530a794b9534
Ngoubou <ralphngoubou@gmail.com>
2022-03-02 00:42:03 +0100

    done

---
+++ week2/ex10_extract_numbers.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+import re
+def extract_numbers(s):
+    return []
+
+def main():
+    print(extract_numbers("abd 123 1.2 test 13.2 -1"))
+
+if __name__ == "__main__":
+    main()
--- week2/ex9_rational.py
+++ week2/ex9_rational.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
-#print(1)
+from fractions import Fraction # for expressing the floats as fractions as instructed
+
 class Rational:
 
     def __init__(self, first, last):
         self.first = first
         self.last = last
 
-    def __repr__(self): # support printing floats
-        return f"{self.first / self.last}"
+    def __str__(self): # support printing floats # can use str or repr
+        return f"{Fraction(self.first / self.last).limit_denominator()}"
     
     # i now implement the basic operations and comparisons
     def __mul__(self, other):
@@ -52,3 +53,51 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# Course Solution ----
+
+# the only time (so far) i prefer my solution
+
+#class Rational(object):    
+ #   def __init__(self, n, d):
+  #      self.n = n
+   #     self.d = d
+
+    #def __str__(self):
+     #   return "%i/%i" % (self.n, self.d)
+
+    #def __repr__(self):
+     #   return "Rational(%i, %i)" % (self.n, self.d)
+
+    #def __mul__(self, r2):
+     #   a = self.n * r2.n
+      #  b = self.d * r2.d
+       # return Rational(a,b)
+
+    #def __truediv__(self, r2):
+     #   a = self.n * r2.d
+      #  b = self.d * r2.n
+       # return Rational(a,b)
+ 
+    #def __add__(self, r2):
+     #   d1 = self.d
+      #  d2 = r2.d
+       # a = self.n * d2 + r2.n * d1
+        #b = d1 * d2
+        #return Rational(a, b)
+
+    #def __sub__(self, r2):
+     #   d1 = self.d
+      #  d2 = r2.d
+       # a = self.n * d2 - r2.n * d1
+        #b = d1 * d2
+        #return Rational(a, b)
+
+    #def __eq__(self, r2):
+     #   return self.n * r2.d == self.d *r2.n
+
+    #def __lt__(self, r2):
+     #   return self.n * r2.d < self.d *r2.n
+
+    #def __gt__(self, r2):
+     #   return self.n * r2.d > self.d *r2.n
