7e05aeaa35a372a03943f61a61d671392b075496
Ngoubou <ralphngoubou@gmail.com>
2022-03-01 15:28:49 +0100

    made a huve advancement

--- playground.py
+++ playground.py
@@ -81,15 +81,29 @@ class MyClass:
 
 ## Corey Schafer YT ----
 class Employee:
+    
+    raise_amt  = 1.04
+
     def __init__(self, first, last, pay):
         self.first = first
         self.last = last
-        self.pay = pay
         self.email = first + "." + last + "@company.com"
+        self.pay = pay
+        
+    #def fullname(self):
+     #   return f"{self.first}, {self.last}"
+    
+    #def apply_raise(self):
+     #   self.pay = int(self.pay * self.raise_amt)
+
+    def __repr__(self):
+        return f"Employee {self.first}, {self.last}, {self.pay}"
+    
     #pass
 
 emp_1 = Employee("Corey", "Schafer", 50000)
 #emp_2 = Employee()
+print(emp_1)
 #print(Employee("Lionel", "Ngoubou", "150K").first)
 #print(emp_2)
 #emp_1.first = "Corey"
@@ -129,8 +143,10 @@ x=C()
 y = B()
 #print(isinstance(x,B)== isinstance(x,C)==isinstance(y,B))
 #print(isinstance(y,C))
-print(5/4)
+#print(5/4)
 x = 5
 y = 4
-print(x.__truediv__(y))
-print(x.__div__(y))
+#print(x.__truediv__(y))
+#print(x.__div__(y))
+
+
--- week2/ex9_rational.py
+++ week2/ex9_rational.py
@@ -1,66 +1,22 @@
 #!/usr/bin/env python3
 
-class Rational(object):
-    """Create a class called Rational"""
-    #x = 0
-    def __init__(self, num1, num2):
-        "This initialises an instance of type ClassName"
-        self.num1 = num1 # creates an instance attribute
-        self.num2 = num2
-        #c = num1 / num2
-    def __truediv__(self):
-        #x += 1
-        print(self.num1.__truediv__(self.num2))
-        #print(x.__truediv__(y))
-        #return s/ss
-        #return self.num1 / self.num2
-    #def div(self):
-        #print(self.num1/self.num2)
-    
-#class test(Rational):
- #   def g(self, a, b):
-        #print(Rational(a, b).__div__())
-  #      Rational(a, b).div()
-    #Rational().div()
-    #print(__truediv__())
-    #print(x)
-#class Test(Rational):
- #   def __init__(self, num1, num2):
-  #      self.num1 = num1 # creates an instance attribute
-   #     self.num2 = num2
-    #print(Rational.__truediv__())
-    #pass
+#print(1)
+class Rational:
 
-## USE CLASS INHERITANCE 
+    def __init__(self, first, last):
+        self.first = first
+        self.last = last
+
+    def __repr__(self):
+        return f"{self.first / self.last}"
+    
 
 def main():
-    r1=Rational(1,4)
-    r2=Rational(2,3)
-    r1.__truediv__()
-    r2.__truediv__()
-    #print(r1)
-   # x = test(1,5)
-    #r1.div()
-    #x.g(1,5)
-    #print(x)
-    #r3 = Test(1,4)
-    #print(r3)
-    #print(r1.__truediv__()) #the result i want
-    #print(r2)
-   # print(r1*r2)
-    #print(r1/r2)
-    #print(r1+r2)
-    #print(r1-r2)
-   # print(Rational(1,2) == Rational(2,4))
-    #print(Rational(1,2) > Rational(2,4))
-    #print(Rational(1,2) < Rational(2,4))
+    r1 = Rational(1,4)
+    r2 = Rational(2, 3)
+    print(r1)
+    print(r2)
+
 
 if __name__ == "__main__":
     main()
-
-# Create a class Rational whose instances are rational numbers. 
-# A new rational number can be created with the call to the class. 
-# For example, the call r=Rational(1,4) creates a rational number “one quarter”. 
-# Make the instances support the following operations: + - * / < > == with their natural behaviour. 
-# Make the rationals also printable so that from the printout 
-# we can clearly see that they are rational numbers. 
