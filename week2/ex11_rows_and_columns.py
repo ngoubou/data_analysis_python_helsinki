976505c77029c57ca2270f30e8c4248556b8f83d
Ngoubou <ralphngoubou@gmail.com>
2022-03-02 11:59:15 +0100

    start ex11

--- week2/ex11_rows_and_columns.py
+++ week2/ex11_rows_and_columns.py
@@ -17,3 +17,20 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# Write two functions, get_rows and get_columns, that get a two dimensional array as parameter. 
+# They should return the list of rows and columns of the array, respectively. 
+# The rows and columns should be one dimensional arrays. 
+# You may use the transpose operation, which flips rows to columns, in your solution. 
+# The transpose is done by the T method:
+# a = np.random.randint(0, 10, (4,4))
+# print(a)
+# print(a.T)
+
+# Test your solution in the main function. Example of usage:
+
+# a = np.array([[5, 0, 3, 3], [7, 9, 3, 5], [2, 4, 7, 6], [8, 8, 1, 6]])
+# get_rows(a)
+# [array([5, 0, 3, 3]), array([7, 9, 3, 5]), array([2, 4, 7, 6]), array([8, 8, 1, 6])]
+# get_columns(a)
+# [array([5, 7, 2, 8]), array([0, 9, 4, 8]), array([3, 3, 7, 1]), array([3, 5, 6, 6])
