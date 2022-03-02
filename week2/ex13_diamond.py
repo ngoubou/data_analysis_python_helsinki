2574fb956da7e34b07ed94fc88352a9aef458da4
Ngoubou <ralphngoubou@gmail.com>
2022-03-02 18:54:40 +0100

    Update ex13_diamond.py

--- week2/ex13_diamond.py
+++ week2/ex13_diamond.py
@@ -10,3 +10,19 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+# Create a function diamond that returns a two dimensional integer array where 
+# the 1s form a diamond shape. Rest of the numbers are 0. 
+# The function should get a parameter that tells the length of a side of the diamond. 
+# Do this using the eye and concatenate functions of NumPy and array slicing.
+
+# Example of usage:
+
+# print(diamond(3))
+# [[0 0 1 0 0]
+# [0 1 0 1 0]
+# [1 0 0 0 1]
+# [0 1 0 1 0]
+# [0 0 1 0 0]]
+# print(diamond(1))
+# [[1]]
