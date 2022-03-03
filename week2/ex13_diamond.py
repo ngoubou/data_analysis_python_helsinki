76dd3144ef1602a6af50b9fee0aaa33e7ba760de
Ngoubou <ralphngoubou@gmail.com>
2022-03-03 19:31:46 +0100

    doing second part of diamond

--- week2/ex13_diamond.py
+++ week2/ex13_diamond.py
@@ -20,19 +20,27 @@ shape = (2 * n - 1,) * 2
 mat_ze = np.zeros(shape, dtype = int)
 #print(mat_ze)
 
-##  3 - Take the line of the zeros matrix and replace it in the identity matrix
+##  3 - Take the line of the identity matrix and replace it in the zeros matrix
 # For how this is done, see my paper shit
-s = 2 * n - 1
-a = s//2
+s = 2 * n - 1 # the shape of the matrix
+a = s//2 # to get the middle array of the identity matrix
 
-for j in range(5): 
-    mat_ze[j] = mat_id[a]
-    print(mat_ze)
-    if j >= 2: # mouvements de remplissage en fonction des lignes (voir feuille papier)
+for i in range(s): 
+    mat_ze[i] = mat_id[a] # start with the middle array of he identity matrix
+    #print(mat_ze)
+    if i >= n - 1: # mouvements de remplissage en fonction des lignes (voir feuille papier)
         a += 1
     else:
         a -= 1
-    
+print(mat_ze)
+
+for j in reversed(range(s)): # range(s-1, 0, -1) is an alternative
+    # pour les modificatiosn j'aurai besoin des indices des "1" de mat_id
+    # créer liste qui va les stocker
+    # puis plutôt que remplacer la ligne, remplacer le chiffre
+    # donc utiliser mat_ze[i,j] = mat_id[i,j]
+    mat_ze[j] = mat_id[a]
+1
 # Faire exactement la même loop mais dans l'autre sens (du bas vers la droite)
 # Look how to incorporate concatenate
     
