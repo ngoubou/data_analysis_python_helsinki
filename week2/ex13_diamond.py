99692ce3af15602ac61c1c0a441d2857a8a22a06
Ngoubou <ralphngoubou@gmail.com>
2022-03-04 09:12:06 +0100

    works but dont use concatenate so

--- week2/ex13_diamond.py
+++ week2/ex13_diamond.py
@@ -12,7 +12,7 @@ if __name__ == "__main__":
     main()
 
 ## 1 - Create a identity matrix with shape(n,n) where n = diamond size length
-n = 3 # we start with a length of 3 as in the example
+n = 6 # we start with a length of 3 as in the example
 mat_id = np.eye(2 * n - 1, dtype = int)
 
 ## 2 - Create a matrix of zeros with the same shape as above
@@ -32,15 +32,28 @@ for i in range(s):
         a += 1
     else:
         a -= 1
-print(mat_ze)
+#print(mat_ze)
+#print(mat_id)
+a = s//2 
 
+# Take the indexes of the ones in the identity matrix
+#print(mat_id)
+ls = [] # store the indexes of each ones, one per array
+for k in mat_id:
+    ls.append(np.where(k == 1)[0][0])
 for j in reversed(range(s)): # range(s-1, 0, -1) is an alternative
     # pour les modificatiosn j'aurai besoin des indices des "1" de mat_id
     # créer liste qui va les stocker
     # puis plutôt que remplacer la ligne, remplacer le chiffre
     # donc utiliser mat_ze[i,j] = mat_id[i,j]
-    mat_ze[j] = mat_id[a]
-1
+    #for k in ls:
+    mat_ze[j, ls[a]] = mat_id[a, ls[a]]
+    #print(mat_ze)
+    if j <= n - 1: # mouvements de remplissage en fonction des lignes (voir feuille papier)
+        a -= 1
+    else:
+        a += 1
+print(mat_ze)
 # Faire exactement la même loop mais dans l'autre sens (du bas vers la droite)
 # Look how to incorporate concatenate
     
