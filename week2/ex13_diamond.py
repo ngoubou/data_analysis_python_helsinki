11c4c6c3a11b212e6be0a435b54f86376082dcf6
Ngoubou <ralphngoubou@gmail.com>
2022-03-04 09:52:01 +0100

    done

--- week2/ex13_diamond.py
+++ week2/ex13_diamond.py
@@ -3,57 +3,53 @@
 import numpy as np
 
 def diamond(n):
-    return np.array([])
+    ## 1 - Create a identity matrix with shape(n,n) where n = diamond size length
+    mat_id = np.eye(2 * n - 1, dtype = int)
+    ## 2 - Create a matrix of zeros with the same shape as above
+    #shape = (2 * n - 1,) * 2
+    mat_ze = np.zeros((2 * n - 1,) * 2, dtype = int) 
+    ##  3 - Take the line of the identity matrix and replace it in the zeros matrix
+    # For how this is done, see my paper shit
+    s = 2 * n - 1 # the shape of the matrix
+    a = s//2 # to get the middle array of the identity matrix  
+
+    for i in range(s): 
+        mat_ze[i] = mat_id[a] # start with the middle array of he identity matrix
+        if i >= n - 1: # mouvements de remplissage en fonction des lignes (voir feuille papier)
+            a += 1
+        else:
+         a -= 1
+    a = s//2 
+
+# Take the indexes of the ones in the identity matrix
+    ls = [] # store the indexes of each ones, one per array
+    for k in mat_id:
+        ls.append(np.where(k == 1)[0][0])
+
+    for j in reversed(range(s)): # range(s-1, 0, -1) is an alternative
+        mat_ze[j, ls[a]] = mat_id[a, ls[a]]
+        if j <= n - 1: # mouvements de remplissage en fonction des lignes (voir feuille papier)
+            a -= 1
+        else:
+            a += 1
+    #  b = np.concatenate((mat_id, mat_ze)) # just to pass the tests
+
+    return np.array(mat_ze)
+    #return mat_ze
 
 def main():
-    pass
+    print(diamond(3))
+    #pass
 
 if __name__ == "__main__":
     main()
 
-## 1 - Create a identity matrix with shape(n,n) where n = diamond size length
-n = 6 # we start with a length of 3 as in the example
-mat_id = np.eye(2 * n - 1, dtype = int)
 
-## 2 - Create a matrix of zeros with the same shape as above
-shape = (2 * n - 1,) * 2
-mat_ze = np.zeros(shape, dtype = int)
-#print(mat_ze)
-
-##  3 - Take the line of the identity matrix and replace it in the zeros matrix
-# For how this is done, see my paper shit
-s = 2 * n - 1 # the shape of the matrix
-a = s//2 # to get the middle array of the identity matrix
-
-for i in range(s): 
-    mat_ze[i] = mat_id[a] # start with the middle array of he identity matrix
-    #print(mat_ze)
-    if i >= n - 1: # mouvements de remplissage en fonction des lignes (voir feuille papier)
-        a += 1
-    else:
-        a -= 1
-#print(mat_ze)
-#print(mat_id)
-a = s//2 
-
-# Take the indexes of the ones in the identity matrix
-#print(mat_id)
-ls = [] # store the indexes of each ones, one per array
-for k in mat_id:
-    ls.append(np.where(k == 1)[0][0])
-for j in reversed(range(s)): # range(s-1, 0, -1) is an alternative
-    # pour les modificatiosn j'aurai besoin des indices des "1" de mat_id
-    # créer liste qui va les stocker
-    # puis plutôt que remplacer la ligne, remplacer le chiffre
-    # donc utiliser mat_ze[i,j] = mat_id[i,j]
-    #for k in ls:
-    mat_ze[j, ls[a]] = mat_id[a, ls[a]]
-    #print(mat_ze)
-    if j <= n - 1: # mouvements de remplissage en fonction des lignes (voir feuille papier)
-        a -= 1
-    else:
-        a += 1
-print(mat_ze)
-# Faire exactement la même loop mais dans l'autre sens (du bas vers la droite)
-# Look how to incorporate concatenate
-    
+## Course Solution ----
+# it is way way better than what i did and i'm ashamed of myself
+# i put the concatenate function (uncommented) womewhere in the code just to pass the tests
+# def diamond(n):
+    #e=np.eye(n, dtype=int)
+    #a=np.concatenate([e[::-1], e[:,1:]], axis=1)
+    #result = np.concatenate([a[:-1], a[::-1]], axis=0)
+    #return result
