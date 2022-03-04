7edd1846bddec4f51dd20a3efe8e66a984e88a47
Ngoubou <ralphngoubou@gmail.com>
2022-03-04 14:51:35 +0100

    summing the array rows

--- week2/ex14_vector_lengths.py
+++ week2/ex14_vector_lengths.py
@@ -15,15 +15,23 @@ if __name__ == "__main__":
 # Exercise 14 (vector lengths)
 
 # Write function vector_lengths that gets a two dimensional array of shape (n,m) as a parameter. 
-a = np.array([[1,2,5], [3,4,0], [9,4,6]])
-b = np.array([7,5,1,8])
-d = np.array([[7], [5], [4]])
-c = np.array([[[7,5,1], [4,6,9], [4,6,7], [4,22,1]]])
-print(a.shape)
-print(d.shape)
-#print(c.shape)
-#print(np.shape(np.array([[1,2,2]])))
 # Each row in this array corresponds to a vector. 
+a = np.array([[2,5], [3,0], [4,6]]) # my input
+b = np.array([1,2,3]) # my output
+c = a.sum(axis = 1)
+n, m = a.shape
+print(a)
+print(a.shape)
+print(c)
+print(c.shape)
+#print([2,5]+ [3,0]+ [4,6])
+#print(f"n = ({n},)")
+
+# What i actually trying to implement is a matrix norm:
+## TO DO ::
+# Look up matrix norm definition
+# Look up scipy.linalg.norm implementation
+
 # The function should return an array of shape (n,), that has the length of each vector in the input. 
 # The length is defined by the usual Euclidean norm. Don't use loops at all in your solution.
 # Instead use vectorized operations. You must use at least the np.sum and the np.sqrt functions. 
