17ab5a84662b916ebb9c12dbc1ead58d2f3b4912
Ngoubou <ralphngoubou@gmail.com>
2022-03-04 12:53:20 +0100

    understanding how array shapes work

--- week2/ex14_vector_lengths.py
+++ week2/ex14_vector_lengths.py
@@ -15,6 +15,14 @@ if __name__ == "__main__":
 # Exercise 14 (vector lengths)
 
 # Write function vector_lengths that gets a two dimensional array of shape (n,m) as a parameter. 
+a = np.array([[1,2,5], [3,4,0], [9,4,6]])
+b = np.array([7,5,1,8])
+d = np.array([[7], [5], [4]])
+c = np.array([[[7,5,1], [4,6,9], [4,6,7], [4,22,1]]])
+print(a.shape)
+print(d.shape)
+#print(c.shape)
+#print(np.shape(np.array([[1,2,2]])))
 # Each row in this array corresponds to a vector. 
 # The function should return an array of shape (n,), that has the length of each vector in the input. 
 # The length is defined by the usual Euclidean norm. Don't use loops at all in your solution.
