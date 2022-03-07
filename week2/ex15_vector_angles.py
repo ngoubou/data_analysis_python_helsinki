3380f8fd15ff5de2e15121a69caa0f6c87499074
Ngoubou <ralphngoubou@gmail.com>
2022-03-07 13:42:04 +0100

    pseudocode

--- week2/ex15_vector_angles.py
+++ week2/ex15_vector_angles.py
@@ -12,8 +12,24 @@ def main():
 if __name__ == "__main__":
     main()
 
+# print(np.cos(1))
+a = np.array([[0,0,1], [-1,1,0]]) # compute the angle between these 2 vectors in degress
+b = np.array([[0,1,0], [1,1,0]])
+#print(a.shape, b.shape)
+s = np.array([0,0,1])
+f = np.array([0,1,0])
+print(np.inner(s,f))
+
+## 1 - Compute the inner product between the vectors of X and Y
+## 2 - Compute the l1 norm of X and Y
+## 3 - Compute the ratio between 1 et 2
+## 4 - Compute the arccos of 3
+## 5 - Profit
+# np.cos()
+# dénominateur = 2 ie, scipy.linalg.norm(a, 1) * scipy.linalg.norm(b, 1)
+
 # Let x and y be m-dimensional vectors. 
-# The angle α\alphaα between two vectors is defined by the equation cos⁡xy(α) = ..., 
+# The angle α between two vectors is defined by the equation cos⁡xy(α) = ..., 
 # where the angle brackets denote the inner product, and ∣∣x∣∣ = ...
 
 # Write function vector_angles that gets two arrays X and Y with same shape (n,m) as parameters. 
