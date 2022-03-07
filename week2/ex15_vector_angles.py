98c19bb4e5ab8d07b2ca16c291e39dbf765525d3
Ngoubou <ralphngoubou@gmail.com>
2022-03-07 14:27:05 +0100

    have to revisit np inner

--- week2/ex15_vector_angles.py
+++ week2/ex15_vector_angles.py
@@ -5,7 +5,7 @@ from scipy.linalg import norm
 from math import pi
 
 def vector_angles(X, Y):
-    
+
     np.array([])
 
 def main():
@@ -17,39 +17,38 @@ if __name__ == "__main__":
 # print(np.cos(1))
 a = np.array([[0,0,1], [-1,1,0]]) # compute the angle between these 2 vectors in degress
 b = np.array([[0,1,0], [1,1,0]])
-#print(a.shape, b.shape)
+print(a.shape, b.shape)
 s = np.array([-1,1,0])
 f = np.array([0,1,0])
 #print(np.inner(s,f))
 
 ## 1 - Compute the inner product between the vectors of X and Y
-prod_in = np.vdot(a, b)
-#print(prod_in)
+prod_in = np.inner(a, b)
+print(prod_in)
 ## 2 - Compute the product of the l1 norms of X and Y
 prod_norms = norm(a, 1) * norm(b, 1)
 #print(prod_norms)
-## 4 - Compute the arccos of the ratio between 1 et 2 and returns the dvalue in degree rather than radian
-angle =  np.arccos(np.clip(prod_in / prod_norms, -1, 1)) * 180 / pi # forces the input to be between -1 and 1 with np.clip
+## 3 - Compute the arccos of the ratio between 1 et 2 and returns the dvalue in degree rather than radian
+
+# NOT IN THE CORRECT FORMAT
+#angle =  np.array([np.arccos(np.clip(prod_in / prod_norms, -1, 1)) * 180 / pi, np.arccos(np.clip(prod_in / prod_norms, -1, 1)) * 180 / pi]) # forces the input to be between -1 and 1 with np.clip
+angle = np.arccos(np.clip(prod_in / prod_norms, -1, 1)) * 180 / pi
 print(angle)
+print(angle.shape)
 
-#print(np.arccos(0)*180/pi)
-#np.arc
-## 5 - Profit
-# np.cos()
-# dénominateur = 2 ie, scipy.linalg.norm(a, 1) * scipy.linalg.norm(b, 1)
+## ERROR FROM TMC
+# # (shapes (2,), (10,) mismatch)
+#x: array([68.566261, 68.566261])
+#y: array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0])
 
-# Let x and y be m-dimensional vectors. 
-# The angle α between two vectors is defined by the equation cos⁡xy(α) = ..., 
-# where the angle brackets denote the inner product, and ∣∣x∣∣ = ...
+## TO DO ----
+# revoir implémentation de np.inner
 
-# Write function vector_angles that gets two arrays X and Y with same shape (n,m) as parameters. 
-# Each row in the arrays corresponds to a vector. The function should return vector of shape (n,) 
-# with the corresponding angles between vectors of X and Y in degrees, not in radians. 
-# Again, don't use loops, but use vectorized operations.
+# Revisit if angle is in the right format
+# my implementation outputs a scaler
+# should it be a vector of different values ofjust the same values repeated multiple times
+# the shape of input is (n,m) and angle should be (n,)
+# il doit y avoir autant de valeurs qu'il y a de vecteurs dans les input array
+# donc les calculs doivent être faits pour chaque vecteur
 
-# Note: function np.arccos is only defined on the domain [-1.0,1.0]. 
-# If you try to compute np.arccos(1.000000001), it will fail.
-# These kind of errors can occur due to use of finite precision in numerical computations. 
-# Force the argument to be in the correct range (clip method).
-
-#Test your solution in the main program.
+# le problème vient de l'étape 1; je devrais juste retourner le 1er array
