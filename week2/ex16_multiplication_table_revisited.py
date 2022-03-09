b97e39b16c1e24a4fd1e419317f8f8a30fcaed37
Ngoubou <ralphngoubou@gmail.com>
2022-03-09 12:46:04 +0100

    done

--- week2/ex16_multiplication_table_revisited.py
+++ week2/ex16_multiplication_table_revisited.py
@@ -3,34 +3,17 @@
 import numpy as np
 
 def multiplication_table(n):
-    return np.array([])
+    return np.arange(n) * np.arange(n).reshape((n,1))
 
 def main():
-    #print(multiplication_table(4))
-    pass
+    print(multiplication_table(4))
+
 if __name__ == "__main__":
     main()
 
-#print(np.arange(4)+4)
-a = np.array([[0, 0, 0, 0], [0, 1, 2, 3], [0, 2, 4, 6], [0, 3, 6, 9]])
-#b = np.random.randn(4,4)
-b = np.random.randint(0, 4, size = (4,4))
-#print(b)
-#print(a[2,3])
-for i in range(len(b)):
-#for i in b:
-    for j in range(len(b[i])):
-        b[i, j] = i * j
-        pass
-# the for loop is what i have to implement relying on broadcasting
-print(b)
-# Write function multiplication_table that gets a positive integer n as parameter. 
-# The function should return an array with shape (n,n). The element at index (i,j) should be i*j. 
-# Don't use for loops! In your solution, rely on broadcasting, the np.arange function, reshaping and vectorized operators.
-# Example of usage:
+## Course Solution ---- 
+# 2nd time i think my solution is better (i should actually time both functions and see the most efficient)
 
-#print(multiplication_table(4))
-#[[0 0 0 0]
-# [0 1 2 3]
-# [0 2 4 6]
-# [0 3 6 9]]
+#def multiplication_table(n):
+#    a=np.arange(n)
+#    return a[:, np.newaxis] * a[np.newaxis, :]
