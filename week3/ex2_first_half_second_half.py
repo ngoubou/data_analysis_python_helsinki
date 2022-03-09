d4a32f732272fd0e63feb04b85aed22ef55a57ce
Ngoubou <ralphngoubou@gmail.com>
2022-03-09 17:28:01 +0100

    done

--- week3/ex2_first_half_second_half.py
+++ week3/ex2_first_half_second_half.py
@@ -3,21 +3,20 @@
 import numpy as np
 
 def first_half_second_half(a):
-    return np.array([])
+    n, m = a.shape
+    m = int(m/2)
+    c = np.sum(a[:,0:m], axis = 1) > np.sum(a[:,-m:], axis = 1)
+    return a[c]
 
 def main():
-    pass
+    a  = np.array([[1, 3, 4, 2], [2, 2, 1, 2]])
+    print(first_half_second_half(a))
 
 if __name__ == "__main__":
     main()
 
-# Write function first_half_second_half that gets a two dimensional array of shape (n,2*m) as a parameter. 
-# The input array has 2*m columns. The output from the function should be a matrix with those rows from 
-# the input that have the sum of the first m elements larger than the sum of the last m elements 
-# on the row. Your solution should call the np.sum function or the corresponding method exactly twice.
-
-# Example of usage:
-
-#a = np.array([[1, 3, 4, 2], [2, 2, 1, 2]])
-#first_half_second_half(a)
-#array([[2, 2, 1, 2]])
+## Course Solution ----
+#def first_half_second_half(a):
+ #   a1, a2 = np.split(a, 2, axis=1)
+  #  mask = np.sum(a1, axis=1) > np.sum(a2, axis=1)
+   # return a[mask]
