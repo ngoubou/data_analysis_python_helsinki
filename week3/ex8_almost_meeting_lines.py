18d9708ae345988b1d8a821859c905f0a0e8b842
Ngoubou <ralphngoubou@gmail.com>
2022-03-10 21:14:20 +0100

    i think it works

--- week3/ex8_almost_meeting_lines.py
+++ week3/ex8_almost_meeting_lines.py
@@ -2,8 +2,27 @@
 
 import numpy as np
 
+#def meeting_lines(a1, b1, a2, b2):
+ #   a = np.array([[-a1,1], [-a2,1]])
+  #  b = np.array([b1,b2])   
+   # return np.linalg.solve(a, b)
+    
 def almost_meeting_lines(a1, b1, a2, b2):
-    return []
+    a = np.array([[-a1,1], [-a2,1]])
+    b = np.array([b1,b2])
+    tt = []
+    c = 0
+    try:
+        t = np.linalg.solve(a, b) # si celui-ci produit une erreur
+        c += 1
+    except np.linalg.LinAlgError:
+        t = np.linalg.lstsq(a,b)[0] # calcule least square
+        #c -= 1 
+    if c > 0:
+        tt.append(True)
+    else:
+        tt.append(False)
+    return t, tt[0]
 
 def main():
     a1=1
@@ -49,7 +68,11 @@ if __name__ == "__main__":
 # Extend that solution so that it tells the meeting point if it exists, 
 # and otherwise finds the point that is closest to the both lines. 
 # You can use the numpy.linalg.lstsq for this.
-
+#a1=1
+#b1=2
+#a2=-1
+#b2=0
+#np.linalg.lsts
 # Example of usage:
 
 # (x, y), exact = almost_meeting_lines(1, 2, -1, 0)
