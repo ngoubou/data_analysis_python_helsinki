0fb7e83b44e0ca197a3769d186e28a551c4e65a7
Ngoubou <ralphngoubou@gmail.com>
2022-03-10 22:12:04 +0100

    start ex9

--- playground.py
+++ playground.py
@@ -231,5 +231,16 @@ def angle_between(v1, v2):
     v2_u = unit_vector(v2)
     return np.arccos(np.clip(np.dot(v1_u, v2_u), -1.0, 1.0))
 
-print(angle_between(a,b))
-np.ra
+#print(angle_between(a,b))
+
+## WEEK 3 ----
+# Matplotlib
+import matplotlib.pyplot as plt
+
+a = np.array([2, 5, 7, 4, 7, 0, 3, 1, 9, 2])
+
+plt.plot(a)                   # plot the points in the array a
+plt.title("My first figure")  # Add a title to the figure
+plt.xlabel("My x-axis")       # Give a label to the x-axis
+plt.ylabel("My y-axis")       # Give a label to the y-axis
+plt.show() 
---
+++ week3/ex9_multiple_graphs.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import matplotlib.pyplot as plt
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
+
+# In the above plot the x coordinates were implicitly set to the indices of the array a, that is, 
+# arange(10). Find out from the documentation of plt.plot how to specify the x coordinates explicitly. 
+# Find out also how to draw multiple graphs in one axes.
+
+# Make your main function plot the following two graphs in one axes. 
+# The first graphs has x coordinates 2,4,6,7 and y coordinates 4,3,5,1. 
+# The second graph has x coordinates 1,2,3,4 and y coordinates 4,2,3,1.
+
+# Add also a title and some labels for x axis and y axis. 
+# Note that in the non-interactive mode you have to call plt.show() for the figure to show.
+
+# The plot should look like the one below.
