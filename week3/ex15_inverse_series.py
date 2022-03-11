0e226a0b70a73fbb31a34c48fbca8cc70cce9a07
Ngoubou <ralphngoubou@gmail.com>
2022-03-11 13:12:55 +0100

    start ex15

---
+++ week3/ex15_inverse_series.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+def inverse_series(s):
+    return pd.Series()
+
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
+
+# Write function inverse_series that get a Series as a parameter and returns a new series, whose indices and values have swapped roles. 
+# Test your function from the main function.
+
+# What happens if some value appears multiple times in the original Series? What happens if you use this value to index the resulting Series?
+
+# One may notice that there are similarities between Python's dictionaries and Pandas' Series, both can be thought to access values using keys. 
+# The difference is that Series requires that the indices have all the same type, and similarly, all the values have the same type.
+# This restriction allows creation of fast data structures.
+
+# As a mark of the similaries between these two data structures, Pandas allows creation of a Series object from a dictionary:
+
+# d = { 2001 : "Bush", 2005: "Bush", 2009: "Obama", 2013: "Obama", 2017 : "Trump"}
+# s4 = pd.Series(d, name="Presidents")
+# s4
+
+# 2001     Bush
+# 2005     Bush
+# 2009    Obama
+# 2013    Obama
+# 2017    Trump
+# Name: Presidents, dtype: object
