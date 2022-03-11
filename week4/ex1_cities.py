f4105c7023ecbb7603cf9656983a723e2b79be5c
Ngoubou <ralphngoubou@gmail.com>
2022-03-11 18:45:02 +0100

    done ex1

--- week4/ex1_cities.py
+++ week4/ex1_cities.py
@@ -3,19 +3,26 @@
 import pandas as pd
 
 def cities():
-    return None
+    df = pd.DataFrame([[643272, 715.48], [643272, 528.03], [231853, 689.59], [223027, 240.35], [201810, 3817.52]], 
+    columns = ["Population", "Total area"], index = ["Helsinki", "Espoo", "Tampere", "Vantaa", "Oulu"])
+    return df
     
 def main():
-    return
+    print(cities())
     
 if __name__ == "__main__":
     main()
 
-# Write function cities that returns the following DataFrame of top Finnish cities by population:
+## Course Solution ----
+# Their solution is more concise and that's normally the way i go about it
 
-#                 Population Total area
-#Helsinki         643272     715.48
-#Espoo            279044     528.03
-#Tampere          231853     689.59
-#Vantaa           223027     240.35
-#Oulu             201810     3817.52
+def cities():
+    a = [[643272, 715.48],
+       [279044, 528.03],
+       [231853, 689.59],
+       [223027, 240.35],
+       [201810, 3817.52]]
+    cols = ["Population", "Total area"]
+    ind = ["Helsinki", "Espoo", "Tampere", "Vantaa", "Oulu"]
+    df = pd.DataFrame(a, index=ind, columns=cols)
+    return df
