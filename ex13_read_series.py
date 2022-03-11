afdf2be9cd5336a625fb98a3fc6526a8a2726441
Ngoubou <ralphngoubou@gmail.com>
2022-03-11 10:48:05 +0100

    completed first part

--- ex13_read_series.py
+++ ex13_read_series.py
@@ -2,7 +2,7 @@
 
 import pandas as pd    # This is the standard way of importing the Pandas library
 import numpy as np
-
+import re
 def read_series():
     return pd.Series()
 
@@ -19,3 +19,15 @@ if __name__ == "__main__":
 # An input line is malformed, if it is non-empty and, when split at whitespace, does not result in two parts.
 
 # Test your function from the main function.
+input = "a Lionel"#\n1 Ralph\n2 Me\n"
+input1 = "b Ralph"
+input2 = "c Me"
+a = [input, input1, input2]
+ind = [] # the series indexes
+val = []
+for i in a:
+    ind.append(re.findall(r'^(.*?)\s', i)[0]) # don't convert it to int cause they can be characters too
+    val.append(re.findall(r'\s(.*?)$', i)[0]) # everything after the space
+
+result = pd.Series(val, index = ind)
+#pd.Se
