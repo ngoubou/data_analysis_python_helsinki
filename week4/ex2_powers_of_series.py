10d0ca6cd877755c4227b29814d287fa80bb8d0a
Ngoubou <ralphngoubou@gmail.com>
2022-03-11 20:11:12 +0100

    start ex2

---
+++ week4/ex2_powers_of_series.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+def powers_of_series(s, k):
+    return None
+    
+def main():
+    return
+    
+if __name__ == "__main__":
+    main()
+
+# Make function powers_of_series that takes a Series and a positive integer k as parameters and returns a DataFrame. 
+# The resulting DataFrame should have the same index as the input Series. 
+# The first column of the dataFrame should be the input Series, the second column should contain the Series raised to power of two. 
+# The third column should contain the Series raised to the power of three, and so on until (and including) power of k. 
+# The columns should have indices from 1 to k.
+
+# The values should be numbers, but the index can have any type. Test your function from the main function. 
+# Example of usage:
+
+
+# Should print:
+
+#    1   2   3
+# a  1   1   1
+# b  2   4   8
+# c  3   9  27
+# d  4  16  64
