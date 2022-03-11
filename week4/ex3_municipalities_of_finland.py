f8d0f413f0e0d3213832f3e520a824fda3e24eb7
Ngoubou <ralphngoubou@gmail.com>
2022-03-11 21:11:56 +0100

    start ex3

---
+++ week4/ex3_municipalities_of_finland.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+def municipalities_of_finland():
+    return None
+    
+def main():
+    return
+    
+if __name__ == "__main__":
+    main()
+
+# In the main function load a data set of municipal information from the src folder (originally from Statistics Finland). 
+# Use the function pd.read_csv, and note that the separator is a tabulator.
+
+# Print the shape of the DataFrame (number of rows and columns) and the column names in the following format:
+
+# Shape: r,c
+# Columns:
+# col1 
+# col2
+# ...
+
+# Note, sometimes file ending tsv (tab separated values) is used instead of csv if the separator is a tab.
