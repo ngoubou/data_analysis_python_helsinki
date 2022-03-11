14e77b2a283e875d1e0c59f2de0ab7cee496cc03
Ngoubou <ralphngoubou@gmail.com>
2022-03-11 13:01:32 +0100

    second function completed

--- week3/ex14_operations_on_series.py
+++ week3/ex14_operations_on_series.py
@@ -8,30 +8,31 @@ def create_series(L1, L2):
     return (s1, s2)
     
 def modify_series(s1, s2):
-    return (None, None)
+    s1["d"] = s2["b"]
+    del s2["b"]
+    return (s1, s2)
     
 def main():
     L1 = [1, 2, 3]
     L2 = [4, 5, 6]
-    print(create_series(L1, L2))
-    return
+    s1 = pd.Series(L1, index = ["a", "b", "c"])
+    s2 = pd.Series(L2, index = ["a", "b", "c"])
+    #print(create_series(L1, L2))
+    print(modify_series(s1, s2))
+    #return
     
 if __name__ == "__main__":
     main()
 
-# Write function create_series that gets two lists of numbers as parameters. 
-# Both lists should have length 3. The function should first create two Series, s1 and s2. 
-# The first series should have values from the first parameter list and have corresponding indices a, b, and c. 
-# The second series should get its values from the second parameter list and have again the corresponding indices a, b, and c. 
-# The function should return the pair of these Series.
+L1 = [1, 2, 3]
+L2 = [4, 5, 6]
+s1 = pd.Series(L1, index = ["a", "b", "c"])
+s2 = pd.Series(L2, index = ["a", "b", "c"])
+s1["d"] = s2["b"]
+del s2["b"]
+#print(s1)
+#print(s2)
 
-# Then, write a function modify_series that gets two Series as parameters. 
-# It should add to the first Series s1 a new value with index d. 
-# The new value should be the same as the value in Series s2 with index b. 
-# Then delete the element from s2 that has index b. 
-# Now the first Series should have four values, while the second list has only two values. 
-# Adding a new element to a Series can be achieved by assignment, like with dictionaries. 
-# Deletion of an element from a Series can be done with the del statement.
 
 # Test these functions from the main function. 
 # Try adding together the Series returned by the modify_series function. 
