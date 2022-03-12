aa2e93fc9d03eb44cf41d52a17e4194b14bf3a85
Ngoubou <ralphngoubou@gmail.com>
2022-03-12 11:29:39 +0100

    start ex5

---
+++ week4/ex5_swedish_and_foreigners.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+def swedish_and_foreigners():
+    return None
+
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
+
+# Write function swedish_and_foreigners that
+
+    # Reads the municipalities data set
+    # Takes the subset about municipalities (like in previous exercise)
+    # Further take a subset of rows that have proportion of Swedish speaking people and proportion of foreigners both above 5 % level
+    # From this data set take only columns about population, the proportions of Swedish speaking people and foreigners, that is three columns.
+
+# The function should return this final DataFrame.
+
+# Do you see some kind of correlation between the columns about Swedish speaking and foreign people? 
+# Do you see correlation between the columns about the population and the proportion of Swedish speaking people in this subset?
