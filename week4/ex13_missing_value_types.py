ccd85b679626acebeafc93d30567954e5ea565a9
Ngoubou <ralphngoubou@gmail.com>
2022-03-12 20:38:32 +0100

    start ex13

---
+++ week4/ex13_missing_value_types.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import numpy as np
+
+def missing_value_types():
+    return None
+               
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
+# Make function missing_value_types that returns the following DataFrame. Use the State column as the (row) index. 
+# The value types for the two other columns should be float and object, respectively. 
+# Replace the dashes with the appropriate missing value symbols.
+
+# State 	Year of independence 	President
+# United Kingdom 	- 	-
+# Finland 	1917 	Niinistö
+# USA 	1776 	Trump
+# Sweden 	1523 	-
+# Germany 	- 	Steinmeier
+# Russia 	1992 	Putin
