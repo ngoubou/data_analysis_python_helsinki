f613e2faeb89110a4cace5a0b7ecfd432103a714
Ngoubou <ralphngoubou@gmail.com>
2022-03-12 13:20:35 +0100

    start ex8

---
+++ week4/ex8_subsetting_by_position.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+def subsetting_by_positions():
+    return pd.DataFrame()
+
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
+
+# Write function subsetting_by_positions that does the following.
+
+# Read the data set of the top forty singles from the beginning of the year 1964 from the src folder. 
+# Return the top 10 entries and only the columns Title and Artist. 
+# Get these elements by their positions, that is, by using a single call to the iloc attribute. 
+# The function should return these as a DataFrame.
