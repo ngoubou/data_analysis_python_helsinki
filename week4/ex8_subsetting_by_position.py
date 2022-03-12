c426c5684815decab59cd29bfbca37ab991a6889
Ngoubou <ralphngoubou@gmail.com>
2022-03-12 13:34:13 +0100

    done

--- week4/ex8_subsetting_by_position.py
+++ week4/ex8_subsetting_by_position.py
@@ -3,17 +3,15 @@
 import pandas as pd
 
 def subsetting_by_positions():
-    return pd.DataFrame()
+    file = "/Users/Mamba/Library/Application Support/tmc/vscode/mooc-data-analysis-with-python-2021/part04-e08_subsetting_by_positions/src/UK-top40-1964-1-2.tsv"
+    df = pd.read_csv(file, sep = "\t")
+    return df.iloc[0:10, [2,3]]
 
 def main():
-    return
+    print(subsetting_by_positions())
 
 if __name__ == "__main__":
     main()
 
-# Write function subsetting_by_positions that does the following.
-
-# Read the data set of the top forty singles from the beginning of the year 1964 from the src folder. 
-# Return the top 10 entries and only the columns Title and Artist. 
-# Get these elements by their positions, that is, by using a single call to the iloc attribute. 
-# The function should return these as a DataFrame.
+## Course Solution ----
+# return df.iloc[:10,2:4]
