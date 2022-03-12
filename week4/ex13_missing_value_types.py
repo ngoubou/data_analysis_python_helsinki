83e3e91cdffa6b7d5ddd98afa6e620dd99712578
Ngoubou <ralphngoubou@gmail.com>
2022-03-12 21:29:01 +0100

    create the dataframe

--- week4/ex13_missing_value_types.py
+++ week4/ex13_missing_value_types.py
@@ -11,6 +11,22 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+state = ["United Kingdom", "Finland", "USA", "Sweden", "Germany", "Russia"] # use it as index
+#columns = {"Year of independence": "-", }
+d = {}
+year = np.array([["-", 1917, 1776, 1523, 1992, "-"], ["-", "Niinistö", "Trump", "-", "Steinmeier", "Putin"]]) # transform "-" as NaN
+#presi = ["-", "Niinistö", "Trump", "-", "Steinmeier", "Putin"]
+cols = ["Year of independence", "President"]
+#print(s)
+dic = {"Year of independence": ["-", 1917, 1776, 1523, 1992, "-"], "President": ["-", "Niinistö", "Trump", "-", "Steinmeier", "Putin"]}
+#print(dic)
+    #for j in year:
+     #   d.update({i: j})
+      #  print(d)
+df = pd.DataFrame(dic, index = state)
+print(df.describe())
+
 # Make function missing_value_types that returns the following DataFrame. Use the State column as the (row) index. 
 # The value types for the two other columns should be float and object, respectively. 
 # Replace the dashes with the appropriate missing value symbols.
