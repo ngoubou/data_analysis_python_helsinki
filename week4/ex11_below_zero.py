be5a8e0be950affca4213d9b9c1c609dfd592c29
Ngoubou <ralphngoubou@gmail.com>
2022-03-12 14:06:08 +0100

    start ex11

---
+++ week4/ex11_below_zero.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+def below_zero():
+    return 0
+
+def main():
+    return
+    
+if __name__ == "__main__":
+    main()
+
+# Write function below_zero that returns the number of days when the temperature was below zero.
+
+# Print the result in the main function in the following form:
+
+# Number of days below zero: xx
