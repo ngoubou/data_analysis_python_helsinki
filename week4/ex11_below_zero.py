95405ba37365aa99566fec61f570a1afac2ded00
Ngoubou <ralphngoubou@gmail.com>
2022-03-12 14:13:57 +0100

    done

--- week4/ex11_below_zero.py
+++ week4/ex11_below_zero.py
@@ -3,16 +3,20 @@
 import pandas as pd
 
 def below_zero():
-    return 0
+    file = "/Users/Mamba/Library/Application Support/tmc/vscode/mooc-data-analysis-with-python-2021/part04-e11_below_zero/src/kumpula-weather-2017.csv"
+    df = pd.read_csv(file)
+    df = df[df["Air temperature (degC)"] < 0]
+    return len(df)
 
 def main():
-    return
-    
+    b_z = below_zero()
+    print(f"Number of days below zero: {b_z}")
+
 if __name__ == "__main__":
     main()
 
-# Write function below_zero that returns the number of days when the temperature was below zero.
 
-# Print the result in the main function in the following form:
-
-# Number of days below zero: xx
+## Course Solution ----
+#def below_zero():
+#    df = pd.read_csv("src/kumpula-weather-2017.csv")
+#    return sum(df["Air temperature (degC)"] < 0.0)
