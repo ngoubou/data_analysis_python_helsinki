de578851c96f5732d2ab6ec79f443fd435f07c3a
Ngoubou <ralphngoubou@gmail.com>
2022-03-12 13:39:03 +0100

    start ex9

---
+++ week4/ex9_snow_depth.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+def snow_depth():
+    return 0.0
+
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
+
+# Write function snow_depth that reads in the weather DataFrame from the src folder and returns the maximum amount of snow in the year 2017.
+
+# Print the result in the main function in the following form:
+
+# Max snow depth: xx.x
+ 
