a5c688b592e71cda1569837ca740fc6d6811cb86
Ngoubou <ralphngoubou@gmail.com>
2022-03-12 09:05:15 +0100

    start ex4

---
+++ week4/ex4_municipalities_of_finland.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+def municipalities_of_finland():
+    return None
+    
+def main():
+    #print(municipalities_of_finland())
+    pass
+    
+if __name__ == "__main__":
+    main()
+
+# Load again the municipal information DataFrame. The rows of the DataFrame correspond to various geographical areas of Finland. 
+# The first row is about Finland as a whole, then rows from Akaa to Äänekoski are municipalities of Finland in alphabetical order. 
+# After that some larger regions are listed.
+
+# Write function municipalities_of_finland that returns a DataFrame containing only rows about municipalities. 
+# Give an appropriate argument for pd.read_csv so that it interprets the column about region name as the (row) index. 
+# This way you can index the DataFrame with the names of the regions.
+
+# Test your function from the main function.
