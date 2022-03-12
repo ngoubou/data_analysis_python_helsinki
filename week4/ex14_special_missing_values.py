08635a955f2641638782681b091db79547b9b6ab
Ngoubou <ralphngoubou@gmail.com>
2022-03-12 21:56:33 +0100

    start ex14

---
+++ week4/ex14_special_missing_values.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+import numpy as np
+
+def special_missing_values():
+    return None
+
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
+
+# Write function special_missing_values that does the following.
+
+# Read the data set of the top forty singles from the beginning of the year 1964 from the src folder. Return the rows whose singles' position dropped compared to last week's position (column LW=Last Week).
+
+# To do this you first have to convert the special values "New" and "Re" (Re-entry) to missing values (None).
