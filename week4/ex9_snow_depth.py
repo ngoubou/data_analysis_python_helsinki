bc95c0d1bf40c50c479275190a12c53debf117a0
Ngoubou <ralphngoubou@gmail.com>
2022-03-12 13:48:07 +0100

    done

--- week4/ex9_snow_depth.py
+++ week4/ex9_snow_depth.py
@@ -3,17 +3,24 @@
 import pandas as pd
 
 def snow_depth():
-    return 0.0
+    file = "/Users/Mamba/Library/Application Support/tmc/vscode/mooc-data-analysis-with-python-2021/part04-e09_snow_depth/src/kumpula-weather-2017.csv"
+    df = pd.read_csv(file)
+    df = df["Snow depth (cm)"]
+    return df.max()
 
 def main():
-    return
+    print(f"Max snow depth: {snow_depth()}")
 
 if __name__ == "__main__":
     main()
 
-# Write function snow_depth that reads in the weather DataFrame from the src folder and returns the maximum amount of snow in the year 2017.
+## Couse Solution ----
+# (Almost) Similar to the course one
 
-# Print the result in the main function in the following form:
-
-# Max snow depth: xx.x
+#def snow_depth():
+#    df = pd.read_csv("src/kumpula-weather-2017.csv")
+#    return df["Snow depth (cm)"].max()
  
+#def main():
+#    max_depth = snow_depth()
+#    print(f"Max snow depth: {max_depth:.1f}")
