d9f0a885750decf0d7748e831102797e38e88c73
Ngoubou <ralphngoubou@gmail.com>
2022-03-12 20:18:05 +0100

    done

--- week4/ex12_cyclists.py
+++ week4/ex12_cyclists.py
@@ -3,19 +3,19 @@
 import pandas as pd
 
 def cyclists():
-    return None
+    file = "/Users/Mamba/Library/Application Support/tmc/vscode/mooc-data-analysis-with-python-2021/part04-e12_cyclists/src/Helsingin_pyorailijamaarat.csv"
+    df = pd.read_csv(file, sep = ";")
+    df = df.dropna(how = "all")
+    df = df.dropna(axis = 1, how = "all")
+    return df
 
 
 def main():
-    return
+    print(cyclists())
     
 if __name__ == "__main__":
     main()
 
-# Write function cyclists that does the following.
 
-# Load the Helsinki bicycle data set from the src folder (https://hri.fi/data/dataset//helsingin-pyorailijamaarat). 
-# The dataset contains the number of cyclists passing by measuring points per hour. 
-# The data is gathered over about four years, and there are 20 measuring points around Helsinki. 
-# The dataset contains some empty rows at the end. Get rid of these. Also, get rid of columns that contain only missing values. 
-# Return the cleaned dataset. 
+## Course Solution ----
+# Same as mine
