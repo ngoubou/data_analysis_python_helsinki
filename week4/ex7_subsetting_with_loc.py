58557c24539a79ef04d5b7610efadb6caf95f3e6
Ngoubou <ralphngoubou@gmail.com>
2022-03-12 13:17:32 +0100

    done

--- week4/ex7_subsetting_with_loc.py
+++ week4/ex7_subsetting_with_loc.py
@@ -3,14 +3,26 @@
 import pandas as pd
 
 def subsetting_with_loc():
-    return None
+    file = "/Users/Mamba/Library/Application Support/tmc/vscode/mooc-data-analysis-with-python-2021/part04-e07_subsetting_with_loc/src/municipal.tsv"
+    df = pd.read_csv(file, sep = "\t", index_col = 0)
+    df = df["Akaa" : "Äänekoski"]
+    rows = []
+    for i in df.index:
+        rows.append(i)
+    cols = ["Population", "Share of Swedish-speakers of the population, %", "Share of foreign citizens of the population, %"]
+    return df.loc[rows, cols]
 
 def main():
-    return
+    print(subsetting_with_loc())
 
 if __name__ == "__main__":
     main()
 
-# Write function subsetting_with_loc that in one go takes the subset of municipalities from Akaa to Äänekoski and restricts it to columns: 
-# "Population", "Share of Swedish-speakers of the population, %", and "Share of foreign citizens of the population, %". 
-# The function should return this content as a DataFrame. Use the attribute loc.
+## Course Solution ----
+# The creation of a variable storing the indexes is unnecessary; my first attempts was not working cause i was writing
+# "Akaa":"Äänekoski" in brackets. So it seems like brackets are only for columns.
+
+#def subsetting_with_loc():
+#    df = pd.read_csv("src/municipal.tsv", index_col=0, sep="\t")
+#    df = df.loc["Akaa":"Äänekoski", ["Population", "Share of Swedish-speakers of the population, %", "Share of foreign citizens of the population, %"]]
+#    return df
