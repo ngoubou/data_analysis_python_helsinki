9233a0809e02e35f39f4448f8824765d47b9bab0
Ngoubou <ralphngoubou@gmail.com>
2022-03-12 12:24:13 +0100

    start ex6

---
+++ week4/ex6_growing_munucipalities.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+def growing_municipalities(df):
+    0.0
+
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
+
+# Write function growing_municipalities that gets subset of municipalities (a DataFrame) as a parameter and returns 
+# the proportion of municipalities with increasing population in that subset.
+
+# Test your function from the main function using some subset of the municipalities. 
+# Print the proportion as percentages using 1 decimal precision.
+
+# Example output:
+
+# Proportion of growing municipalities: 12.4%
