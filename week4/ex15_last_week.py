f764fbc352bec07f972293ba2e208023a4febd55
Ngoubou <ralphngoubou@gmail.com>
2022-03-14 15:58:41 +0100

    Update ex15_last_week.py

--- week4/ex15_last_week.py
+++ week4/ex15_last_week.py
@@ -12,7 +12,7 @@ def last_week():
 
     missing = df[df.isnull().any(axis = 1)]
     new_df = lw.append(missing, ignore_index = True)
-    new_df.sort_values("LW", ascending = True)
+    new_df.sort_values("LW", ascending = True, inplace = True)
     return new_df
 
 def main():
@@ -27,7 +27,13 @@ if __name__ == "__main__":
 
 
 # Write function last_week that reads the top40 data set mentioned in the above exercise. 
+# The function should then try to reconstruct the top40 list of the previous week based on that week's list. 
+# Try to do this as well as possible. You can fill the values that are impossible to reconstruct by missing value symbols. 
+# Your solution should work for a top40 list of any week. So don't rely on specific features of this top40 list. 
+# The column WoC means "Weeks on Chart", that is, on how many weeks this song has been on the top 40 list.
 
+
+# Hint. First create the last week's top40 list of those songs that are also on this week's list. 
 # Then add those entries that were not on this week's list. Finally sort by position.
 
 # Hint 2. The where method of Series and DataFrame can be useful. It can also be nested.
