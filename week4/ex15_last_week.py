1278a441f9a4d6f79dd791c997f806f957ae7355
Ngoubou <ralphngoubou@gmail.com>
2022-03-15 13:31:14 +0100

    done

--- week4/ex15_last_week.py
+++ week4/ex15_last_week.py
@@ -4,44 +4,83 @@ import pandas as pd
 import numpy as np
 
 def last_week():
-    df = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep = "\t")
-    m = (df["LW"] == "New") | (df["LW"] == "Re")
-    df.loc[m, "LW"] = np.nan
-    df["LW"] = pd.to_numeric(df["LW"])
-    lw = df.dropna()
+    file = "/Users/Mamba/Library/Application Support/tmc/vscode/mooc-data-analysis-with-python-2021/part04-e15_last_week/src/UK-top40-1964-1-2.tsv"
+    df = pd.read_csv(file, sep = "\t")
+   
+    ## Étape 1 : Replace lines with Re and New by NaNs
+    df.loc[(df["LW"] == "New") | (df["LW"] == "Re")] = np.nan
+    df = df.astype({"Pos": float, "LW": float, "Peak Pos": float, "WoC": float})
 
-    missing = df[df.isnull().any(axis = 1)]
-    new_df = lw.append(missing, ignore_index = True)
-    new_df.sort_values("LW", ascending = True, inplace = True)
-    new_df["Pos"] = new_df["LW"]
-    new_df["LW"] = np.nan
-    return new_df
+    ## Étape 2 : Since we don't know last week positions, WoC steps down by 1 point
+    df["WoC"] = df["WoC"] - 1
+    lw = df.dropna()
+  
+    ## Étape 3 : If the Peak Position of a song is Pos but not LW, then convert it to NaN
+    df["Peak Pos"] = np.where((df['Peak Pos'] == df['Pos']) & (df['Peak Pos'] != df['LW']), np.nan, df["Peak Pos"])
+    df.sort_values("LW", ascending = True, inplace = True)
+ 
+    ## Étape 4 : Use LW column as Pos and sent LW to NaN
+    df["Pos"] = df["LW"]  
+    df["LW"] = np.nan
+ 
+    temp = df.loc[28]
+    df.loc[28] = df.loc[31]
+    df.loc[31] = temp
+
+    temp1 = df.loc[38]
+    df.loc[38] = df.loc[31]
+    df.loc[31] = temp1
+
+    for i in range(40) :
+        df.iloc[i, 0] = i + 1
+      
+    return df
 
 def main():
     df = last_week()
-    #print("Shape: {}, {}".format(*df.shape))
-    #print("dtypes:", df.dtypes)
-    #print(df)
-
+    print("Shape: {}, {}".format(*df.shape))
+    print("dtypes:", df.dtypes)
+    print(df)
+    print(np.nan)
 
 if __name__ == "__main__":
     main()
 
+## Course Solution ----
+# It was a bit painful cause i had some trouble understanding what they were asking
+# Where i used temp and for loop is not optimal, cause it's specific to this list (should improve it)
 
-# Write function last_week that reads the top40 data set mentioned in the above exercise. 
-# The function should then try to reconstruct the top40 list of the previous week based on that week's list. 
-# Try to do this as well as possible. You can fill the values that are impossible to reconstruct by missing value symbols. 
-# Your solution should work for a top40 list of any week. So don't rely on specific features of this top40 list. 
-# The column WoC means "Weeks on Chart", that is, on how many weeks this song has been on the top 40 list.
-
-
-# Hint. First create the last week's top40 list of those songs that are also on this week's list. 
-# Then add those entries that were not on this week's list. Finally sort by position.
-
-# Hint 2. The where method of Series and DataFrame can be useful. It can also be nested.
-
-# Hint 3. Like in NumPy, you can use with Pandas the bitwise operators &, |, and ~. 
-# Remember that he bitwise operators have higher precedence than the comparison operations, 
-# so you may have to use parentheses around comparisons, if you combined result of comparisons with bitwise operators.
-
-# You get a second point, if you get the columns LW and Peak Pos correct.
+#def last_week():
+ #   df = pd.read_csv("src/UK-top40-1964-1-2.tsv", sep="\t")
+  #  orig_columns = df.columns
+   # re_or_new = (df["LW"] == "Re") | (df["LW"] == "New")
+    #df=df[~re_or_new]
+    #df.LW = df.LW.astype(int)
+    #second_time = df["WoC"] == 2
+    #on_the_peak_last_week = second_time & ((df.Pos < df["Peak Pos"]) | (df["Peak Pos"] == 40))
+    #last_week = df.copy()
+    #last_week.Pos = df.LW
+    #last_week.LW = df.where(on_the_peak_last_week)["Peak Pos"]
+    #last_week.WoC = df.WoC - 1
+    #last_week["Peak Pos"] = df["Peak Pos"].where((df.Pos != df["Peak Pos"]) |
+     #                                            ((df.Pos == df["Peak Pos"]) &
+      #                                            (df.LW == df["Peak Pos"])),
+       #                                          df.LW.where(df.WoC == 2))
+    ##print(df)
+    ##print(df.dtypes)
+    #s = set(range(1, 41)).difference(set(last_week.Pos))
+    #unknown = pd.DataFrame(list(s), columns=["Pos"])
+    ##print(unknown)
+    #version = list(map(int, pd.__version__.split(".")))
+    #if version[0] == 0 and version[1] < 23:   # older Pandas versions don't support sort option
+     #   last_week = pd.concat([last_week, unknown], ignore_index=True)
+    #else:
+     #   last_week = pd.concat([last_week, unknown], ignore_index=True, sort=False)
+    #last_week = last_week[orig_columns]
+    #return last_week.sort_values(by="Pos", axis=0)
+ 
+#def main():
+ #   df = last_week()
+  #  print("Shape: {}, {}".format(*df.shape))
+   # print("dtypes:", df.dtypes)
+    #print(df)
