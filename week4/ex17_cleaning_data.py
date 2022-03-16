c3f731bc1cac9842d4b8a09a8cc0e4a7612b88e7
Ngoubou <ralphngoubou@gmail.com>
2022-03-16 11:59:52 +0100

    done

--- week4/ex17_cleaning_data.py
+++ week4/ex17_cleaning_data.py
@@ -5,67 +5,59 @@ import numpy as np
 import re
 
 def cleaning_data():
-    return None
+    file = "/Users/Mamba/Library/Application Support/tmc/vscode/mooc-data-analysis-with-python-2021/part04-e17_cleaning_data/src/presidents.tsv"
+    df = pd.read_csv(file, sep = "\t")
+
+    # Season modif
+    df.replace(to_replace = "two", value = 2, inplace = True)
+
+    # Last modif
+    df.replace(to_replace = "-", value = np.nan, inplace = True)
+
+    # Start modif
+    start = df["Start"].str.split(" ")
+    for i, k in enumerate(df["Start"]):
+        j = start[i]
+        df["Start"].replace(to_replace = k, value = j[0], inplace = True)
+
+    # President modif  
+    for i in df["President"]:
+        if re.findall(r',\s', i):
+            j = i.split(", ")
+            j.reverse()
+            j = " ".join(j)
+            df["President"].replace(to_replace = i, value = j, inplace = True)
+
+    # Vice-president modif
+    df["Vice-president"] = df["Vice-president"].str.title()
+    for i in df["Vice-president"]:
+        if re.findall(r',\s', i):
+            j = i.split(", ")
+            j.reverse()
+            j = " ".join(j)
+            df["Vice-president"].replace(to_replace = i, value = j, inplace = True)
+
+    # Type conversion
+    df = df.astype({"Start": int, "Last": float, "Seasons": int})
+    return df
 
 def main():
-    return
+    print(cleaning_data())
 
 if __name__ == "__main__":
     main()
 
-# This exercise can give two points at maximum!
+## Course Solution ----
 
-# The entries in the following table of US presidents are not uniformly formatted. 
-# Make function cleaning_data that reads the table from the tsv file src/presidents.tsv and returns the cleaned version of it. 
-file = "/Users/Mamba/Library/Application Support/tmc/vscode/mooc-data-analysis-with-python-2021/part04-e17_cleaning_data/src/presidents.tsv"
-df = pd.read_csv(file, sep = "\t")
-#print(df.dtypes)
+#def fix_name_field(s):
+ #   s = s.str.replace(r"(\w+), *(\w+)", r"\2 \1")
+  #  return s
 
-# Seasons modif
-df.replace(to_replace = "two", value = 2, inplace = True)
-
-# Last modif
-df.replace(to_replace = "-", value = np.nan, inplace = True)
-
-# Start modif
-start = df["Start"].str.split(" ")
-for i, k in enumerate(df["Start"]):
-    j = start[i]
-    df["Start"].replace(to_replace = k, value = j[0], inplace = True)
-
-# President modif  
-for i in df["President"]:
-    if re.findall(r',\s', i):
-        #print(i)
-        j = i.split(", ")
-        j.reverse()
-        j = " ".join(j)
-        df["President"].replace(to_replace = i, value = j, inplace = True)
-        #print(df["President"])
-
-# Vice-president modif
-df["Vice-president"] = df["Vice-president"].str.title()
-#print(df["Vice-president"])
-
-for i in df["Vice-president"]:
-   
-    if re.findall(r',\s', i):
-        #print(i)
-        j = i.split(", ")
-        j.reverse()
-        j = " ".join(j)
-        df["Vice-president"].replace(to_replace = i, value = j, inplace = True)
-#print(df)
-# Faire à la fin
-df = df.astype({"Start": int, "Last": float, "Seasons": int})
-print(df.dtypes)
-# Note, you must do the edits programmatically using the string edit methods, not by creating a new DataFrame by hand.
-# The columns should have dtypes object, integer, float, integer, object. 
-# The where method of DataFrames can be helpful, likewise the string methods of Series objects. 
-# You get an additional point, if you manage to get the columns President and Vice-president right!
-
-#President 	Start 	Last 	Seasons 	Vice-president
-#donald trump 	2017 Jan 	- 	1 	Mike pence
-#barack obama 	2009 	2017 	2 	joe Biden
-#bush, george 	2001 	2009 	2 	Cheney, dick
-#Clinton, Bill 	1993 	2001 	two 	gore, Al
+#def cleaning_data():
+ #   df = pd.read_csv("src/presidents.tsv", sep="\t")
+  #  df = df.where(df != "two", 2)
+   # df['Start'] = df['Start'].str.extract(r'^(\d{4})', expand=False)
+    #df = df.where(df != "-", np.nan)
+    #df["President"] = fix_name_field(df["President"])
+    #df["Vice-president"] = fix_name_field(df["Vice-president"]).str.title()
+    #return df.astype({"President":object, "Start":int,  "Last":float, "Seasons":int, "Vice-president": object})
