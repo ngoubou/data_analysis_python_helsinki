f80b6eda0efcf5abd4dca9f6626f47d8b808593a
Ngoubou <ralphngoubou@gmail.com>
2022-03-16 18:09:19 +0100

    start ex1

---
+++ week5/ex1_split_date_continues.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+
+def split_date(df):
+    return df
+
+def split_date_continues():
+    return None
+
+def main():
+    df = split_date_continues()
+    print("Shape:", df.shape)
+    print("Column names:\n", df.columns)
+    print(df.head())
+
+
+if __name__ == "__main__":
+    main()
+
+# Write function split_date_continues that does
+
+    # read the bicycle data set
+    # clean the data set of columns/rows that contain only missing values
+    # drops the Päivämäärä column and replaces it with its splitted components as before
+
+# Use the concat function to do this. The function should return a DataFrame with 25 columns (first five related to the date and then the rest 20 conserning the measument location.
+
+# Hint: You may use your solution or the model solution from exercise 16 of the previous set as a starting point.
