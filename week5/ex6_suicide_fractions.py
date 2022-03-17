15ed188671331af990ade0d1820d0fc00c49f0b9
Ngoubou <ralphngoubou@gmail.com>
2022-03-17 15:14:34 +0100

    Update ex6_suicide_fractions.py

--- week5/ex6_suicide_fractions.py
+++ week5/ex6_suicide_fractions.py
@@ -9,24 +9,37 @@ new_dir = os.chdir("/Users/mamba/Downloads/Data_Scientist_Path/Courses/python_he
 def suicide_fractions():
     df = pd.read_csv("data/who_suicide_statistics.csv")
     groups = df.groupby("country")
-    mean_fraction = round(groups["suicides_no"].mean() / groups["population"].mean(), 6)
+    mean_fraction = groups["suicides_no"].mean() / groups["population"].mean()
     return mean_fraction
 
 def main():
-    print(suicide_fractions().head())
+    pass
+    #print(suicide_fractions())
 
+if __name__ == "__main__":
+    main()
 # Load the suicide data set from src folder. This data was originally downloaded from Kaggle. 
 # Kaggle contains lots of interesting open data sets.
-#df = pd.read_csv("data/who_suicide_statistics.csv")
+df = pd.read_csv("data/who_suicide_statistics.csv")
 #df = df.dropna()
 #print(df.shape)#.dtypes)
 #print(df.dropna().shape)
-#groups = df.groupby("country")
+groups = df.groupby("country")
 
-#print(groups["suicides_no"].mean() / groups["population"].mean())
-#mean_fraction = groups["suicides_no"].mean() / groups["population"].mean()
+#suic = groups["suicides_no"].mean()
+#pop = groups["population"].mean()
+#print(suic)
+#print(pop)
+#print(suic / pop)
+mean_fraction = groups["suicides_no"].mean() / groups["population"].mean()
+#a =  groups["suicides_no"].sum() / groups["population"].sum()
+df1 = df.dropna()
+df2 = df1["suicides_no"] / df1["population"]
+print(type(df2))
+#print(mean_fraction)
 #print(groups["population"].mean())
 #print(7.462121/2.338514e+05)
+#print(7.462121/233851.4)
 ## EN DERNIER ----
 #df = df.drop(["year", "sex", "age"], axis = 1)
 
