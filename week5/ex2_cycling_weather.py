a528c62f55a33bf6fbe4ce6e682455033bfb9147
Ngoubou <ralphngoubou@gmail.com>
2022-03-17 08:06:33 +0100

    start ex2

---
+++ week5/ex2_cycling_weather.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+
+def cycling_weather():
+    return None
+
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
+
+# Merge the processed cycling data set (from the previous exercise) and weather data set along the columns year, month, and day. 
+# Note that the names of these columns might be different in the two tables: use the left_on and right_on parameters. 
+# Then drop useless columns 'm', 'd', 'Time', and 'Time zone'.
+
+# Write function cycling_weather that reads the data sets and returns the resulting DataFrame.
