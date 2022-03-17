4e1034f865280b784cd76855184c769ab95a57ca
Ngoubou <ralphngoubou@gmail.com>
2022-03-17 12:13:20 +0100

    start ex5

---
+++ week5/ex5_best_record_company.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+def best_record_company():
+    return None
+
+def main():
+    return
+    
+
+if __name__ == "__main__":
+    main()
+
+# We use again the UK top 40 data set from the first week of 1964 in the src folder. 
+# Here we define "goodness" of a record company (Publisher) based on the sum of the weeks on chart (WoC) of its singles. 
+# Return a DataFrame of the singles by the best record company (a subset of rows of the original DataFrame). 
+# Do this with function best_record_company.
