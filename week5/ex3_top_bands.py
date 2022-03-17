8c30ed949edc4b413e6bad89adad9c86ae832d79
Ngoubou <ralphngoubou@gmail.com>
2022-03-17 09:18:12 +0100

    start ex3

---
+++ week5/ex3_top_bands.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+def top_bands():
+    return pd.DataFrame()
+
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
+
+# Merge the DataFrames UK top40 and the bands DataFrame that are stored in the src folder. 
+# Do all this in the parameterless function top_bands, which should return the merged DataFrame. 
+# Use the left_on and right_on parameters to merge. Test your function from the main function.
