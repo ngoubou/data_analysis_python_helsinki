8d8ae07e5cdb4ac554fbb1cfecad9baf4df61968
Ngoubou <ralphngoubou@gmail.com>
2022-03-17 09:13:52 +0100

    done

--- week5/ex2_cycling_weather.py
+++ week5/ex2_cycling_weather.py
@@ -10,30 +10,48 @@ new_dir = os.chdir("/Users/mamba/Downloads/Data_Scientist_Path/Courses/python_he
 from ex1_split_date_continues import split_date_continues
 
 def cycling_weather():
-    return None
+    df = split_date_continues()
+    file1 = "/Users/mamba/Downloads/Data_Scientist_Path/Courses/python_helsinki/week5/data/kumpula-weather-2017.csv"
+    weather = pd.read_csv(file1)
+    merged = pd.merge(weather, df,  left_on = ["Year", "d", "m"], right_on = ["Year", "Day", "Month"])
+    merged = merged.drop(['m', 'd', 'Time', 'Time zone'], axis = 1)
+    return merged
 
 def main():
-    return
+    df = cycling_weather()
+    print("Shape:", df.shape)
 
 if __name__ == "__main__":
     main()
 
-# Merge the processed cycling data set (from the previous exercise) and weather data set along the columns year, month, and day. 
-#file = "/Users/mamba/Downloads/Data_Scientist_Path/Courses/python_helsinki/week5/data/Helsingin_pyorailijamaarat.csv"
-file1 = "/Users/mamba/Downloads/Data_Scientist_Path/Courses/python_helsinki/week5/data/kumpula-weather-2017.csv"
-df = split_date_continues()
-#print(df.dtypes)
-#cycle = pd.read_csv(file)
-weather = pd.read_csv(file1)
-#print("cycle shape ", cycle.shape)
-#print("---------------------------------------------------------------------")
-#print(weather.dtypes)
-#pd.mer
-merged = pd.merge(weather, df,  left_on = ["Year", "d", "m"], right_on = ["Year", "Day", "Month"])
-merged = merged.drop(['m', 'd', 'Time', 'Time zone'], axis = 1)
-print(merged.shape)
-print(merged.dtypes)
-# Note that the names of these columns might be different in the two tables: use the left_on and right_on parameters. 
-# Then drop useless columns 'm', 'd', 'Time', and 'Time zone'.
+## Course solution
+# My solution is more concise than the one submitted (and also than the course one)
+# cause i take advantage of modules, ie i imported the functions written in a previous assignment
+# rather than re-writing them.
 
-# Write function cycling_weather that reads the data sets and returns the resulting DataFrame.
+#import pandas as pd
+#days = dict(zip("ma ti ke to pe la su".split(), "Mon Tue Wed Thu Fri Sat Sun".split()))
+#months = dict(zip("tammi helmi maalis huhti touko kesä heinä elo syys loka marras joulu".split(), range(1, 13)))
+
+#def split_date(df):
+    #d = df["Päivämäärä"].str.split(expand=True)
+    #d.columns = ["Weekday", "Day", "Month", "Year", "Hour"]
+    #hourmin = d["Hour"].str.split(":", expand=True)
+    #d["Hour"] = hourmin.iloc[:, 0]
+   # d["Weekday"] = d["Weekday"].map(days)
+  #  d["Month"] = d["Month"].map(months)
+ #   d = d.astype({"Weekday": object, "Day": int, "Month": int, "Year": int, "Hour": int})
+#    return d
+
+#def split_date_continues():
+    #df = pd.read_csv("src/Helsingin_pyorailijamaarat.csv", sep=";")
+    #df = df.dropna(axis=0, how="all").dropna(axis=1, how="all")
+   # d = split_date(df)
+  #  df = df.drop("Päivämäärä", axis=1)
+ #   return pd.concat([d, df], axis=1)
+
+#def cycling_weather():
+    #wh = pd.read_csv("src/kumpula-weather-2017.csv")
+    #bike = split_date_continues()
+    #result = pd.merge(wh, bike, left_on=["Year", "m", "d"], right_on=["Year", "Month", "Day"])
+    #return result.drop(['m', 'd', 'Time', 'Time zone'], axis=1)
