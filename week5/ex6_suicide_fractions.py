647677422612e6df1a16ea2dc434282d044d9ab1
Ngoubou <ralphngoubou@gmail.com>
2022-03-17 15:32:56 +0100

    done

--- week5/ex6_suicide_fractions.py
+++ week5/ex6_suicide_fractions.py
@@ -13,36 +13,22 @@ def suicide_fractions():
     return mean_fraction
 
 def main():
-    pass
-    #print(suicide_fractions())
+    print(suicide_fractions())
 
 if __name__ == "__main__":
     main()
-# Load the suicide data set from src folder. This data was originally downloaded from Kaggle. 
-# Kaggle contains lots of interesting open data sets.
-df = pd.read_csv("data/who_suicide_statistics.csv")
-#df = df.dropna()
-#print(df.shape)#.dtypes)
-#print(df.dropna().shape)
-groups = df.groupby("country")
 
-#suic = groups["suicides_no"].mean()
-#pop = groups["population"].mean()
-#print(suic)
-#print(pop)
-#print(suic / pop)
-mean_fraction = groups["suicides_no"].mean() / groups["population"].mean()
-#a =  groups["suicides_no"].sum() / groups["population"].sum()
-df1 = df.dropna()
-df2 = df1["suicides_no"] / df1["population"]
-print(type(df2))
-#print(mean_fraction)
-#print(groups["population"].mean())
-#print(7.462121/2.338514e+05)
-#print(7.462121/233851.4)
-## EN DERNIER ----
-#df = df.drop(["year", "sex", "age"], axis = 1)
+## Course Solution
+# My solution was almost correct (to 6 decimal places)
+# But the course colution makes a bit more sense
+# They first created a new column computing the suicide fraction
+# then they group by country before computing the mean
 
-# Write function suicide_fractions that loads the data set and returns a Series 
-# that has the country as the (row) index and as the column the mean fraction of suicides per population in that country. 
-# In other words, the value is the average of suicide fractions. The information about year, sex and age is not used.
+# The solution submitted is not mine (got it on discord(@sanni))
+
+#def suicide_fractions():
+ #   df = pd.read_csv("src/who_suicide_statistics.csv")
+  #  df["Suicide fraction"] = df["suicides_no"] / df["population"]
+   # print(df)
+    #result = df.groupby("country").mean()
+    #return result["Suicide fraction"]
