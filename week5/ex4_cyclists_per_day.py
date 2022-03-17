42a3e1dd683c46ce9d38570b76289dcf1c58850b
Ngoubou <ralphngoubou@gmail.com>
2022-03-17 10:46:41 +0100

    first part works

--- week5/ex4_cyclists_per_day.py
+++ week5/ex4_cyclists_per_day.py
@@ -2,8 +2,12 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
+# change directory
+new_dir = os.chdir("/Users/mamba/Downloads/Data_Scientist_Path/Courses/python_helsinki/week5")
 
-
+#from ex2_cycling_weather import cycling_weather
+from ex1_split_date_continues import split_date_continues
 def cyclists_per_day():
     return None
     
@@ -18,6 +22,17 @@ if __name__ == "__main__":
 # Part 1.
 
 # Read, clean and parse the bicycle data set as before. Group the rows by year, month, and day. Get the sum for each group. 
+file = "/Users/mamba/Downloads/Data_Scientist_Path/Courses/python_helsinki/week5/data/kumpula-weather-2017.csv"
+#df = pd.read_csv("data/Helsingin_pyorailijamaarat.csv")
+#df = cycling_weather()
+df1 = split_date_continues()
+#print(df.shape)
+#print(df1.shape)
+#df = df.drop(["Hour", "Weekday"], axis = 1)
+df1 = df1.drop(["Hour", "Weekday"], axis = 1)
+#print(df1.shape)
+groups = df1.groupby(["Year", "Month", "Day"])
+print(groups.sum().dtypes)
 # Make function cyclists_per_day that does the above. The function should return a DataFrame. 
 # Make sure that the columns Hour and Weekday are not included in the returned DataFrame.
 
