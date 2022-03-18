6f6c1e2f7bbd20f7e2847bd3e5a010d419216abc
Ngoubou <ralphngoubou@gmail.com>
2022-03-18 20:32:51 +0100

    start ex10

---
+++ week5/ex10_linear_regression.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+
+def fit_line(x, y):
+    return 0, 0
+    
+def main():
+    
+if __name__ == "__main__":
+    main()
+# This exercise can give two points at maximum!
+
+# Part 1.
+
+# Write a function fit_line that gets one dimensional arrays x and y as parameters. The function should return the tuple (slope, intercept) of the fitted line. Write a main program that tests the fit_line function with some example arrays. The main function should produce output in the following form:
+
+# Slope: 1.0
+# Intercept: 1.16666666667
+
+# Part 2.
+
+# Modify your main function to plot the fitted line using matplotlib, in addition to the textual output. Plot also the original data points.
