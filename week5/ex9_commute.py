8a6ffdca58044eb0db9b3774c666853363f5510e
Ngoubou <ralphngoubou@gmail.com>
2022-03-18 16:41:59 +0100

    Update ex9_commute.py

--- week5/ex9_commute.py
+++ week5/ex9_commute.py
@@ -2,13 +2,36 @@
 
 import pandas as pd
 import matplotlib.pyplot as plt
+import os 
 
+os.chdir("/Users/mamba/Downloads/Data_Scientist_Path/Courses/python_helsinki/week5")
+from ex8_bicycle_timeseries import bicycle_timeseries
 
 def commute():
-    return None
+    df1 = bicycle_timeseries()
+    df = pd.read_csv("data/Helsingin_pyorailijamaarat.csv", sep = ";")
+    df = df.dropna(axis = 0, how="all").dropna(axis = 1, how = "all")
+    df = df.drop("Päivämäärä", axis = 1)
+    august = pd.date_range("2017-08-01", "2017-08-31")#, freq="w-mon")
+    df1 = df1.loc[august,:]
+    df1.reset_index(inplace = True)
+    df1["Weekday"] = df1["Date"].dt.dayofweek
+    df1["Weekday"] += 1 # Set the Weekday column to numbers from one to seven.
+    groups = df1.groupby("Weekday").sum()#.reset_index(inplace = True)
+    #groups.reset_index(inplace = True)
+    return groups
     
 def main():
-    return
+    df = commute()
+    plt.plot(df)
+    plt.suptitle("Daily bike rentals in Helsinki") # This is actually the title
+    #plt.title("by station") # the subtitle
+    plt.xlabel(range(len(df.index)))      
+    plt.ylabel("Total rentals")       
+    #plt.xticks(np.arange(1,32))
+    #plt.legend(title = "Bike stations", loc = "upper right") # nothing shows up
+    plt.show()
+    #print(df)
 
 
 if __name__ == "__main__":
