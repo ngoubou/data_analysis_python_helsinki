92bcae534e690df2bdcc1e73658c9ecd5481a81f
Ngoubou <ralphngoubou@gmail.com>
2022-03-18 17:45:58 +0100

    Update ex7_suicide_weather.py
    totally done

--- week5/ex7_suicide_weather.py
+++ week5/ex7_suicide_weather.py
@@ -36,3 +36,45 @@ def main():
 
 if __name__ == "__main__":
     main()
+
+## Course Solution ----
+# Turns out that the correlation difference was due to the fact that i was using directly the wikipedia link,
+# rather than the html file provided by the course. 
+# And thanks to someone on discord, i changed that. And when i dit i was getting an error cause
+# i was not specifying the whole path for the files. 
+# This is certainly because i was not in the exercise directory.
+# And after changing that, i noticed that the column names differed from the html file provided with the wiki link.
+
+#def suicide_fractions():
+  #  df = pd.read_csv("src/who_suicide_statistics.csv")
+  #  df["Suicide fraction"] = df["suicides_no"] / df["population"]
+ #   result = df.groupby("country").mean()
+#    return result["Suicide fraction"]
+
+#def info(df, name):
+    #print("\n%s" % name)
+    #print("=" * len(name))
+    #print("Shape:", df.shape)
+   # print("dtypes:\n", df.dtypes)
+  #  if isinstance(df, pd.DataFrame):
+ #       print("column names:", df.columns)
+#    print(df.head())
+
+#def suicide_weather():
+    #suicide = suicide_fractions()
+    #version = list(map(int, pd.__version__.split(".")))   # Get version of Pandas
+    # older Pandas versions don't support displayed_only option 
+    #if version[0] == 0 and version[1] < 23:   
+     #   tables = pd.read_html("src/List_of_countries_by_average_yearly_temperature.html", header=0, index_col=0)
+    #else:
+     #   tables = pd.read_html("src/List_of_countries_by_average_yearly_temperature.html", header=0, index_col=0, displayed_only=False)
+    #temperature = tables[1]    # The first table is a non-displayed one, we don't want that
+    #temperature = pd.to_numeric(temperature.iloc[:, 0].str.replace("\u2212", "-"))
+    ##info(suicide, "Suicide fractions")
+    ##info(temperature, "Temperatures")
+    #corr = suicide.corr(temperature, method="spearman")
+    #if version[0] == 0 and version[1] < 23:   # older Pandas versions don't support sort option
+     #   common = pd.concat([suicide, temperature], axis=1, join="inner")
+    #else:
+     #   common = pd.concat([suicide, temperature], axis=1, join="inner", sort=False)
+    #return (suicide.shape[0], temperature.shape[0], common.shape[0], corr)
