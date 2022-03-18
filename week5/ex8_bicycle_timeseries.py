4f3b9bbbb5e8ca37c4bb93b689c06b242de296c3
Ngoubou <ralphngoubou@gmail.com>
2022-03-18 14:51:51 +0100

    start ex8

---
+++ week5/ex8_bicycle_timeseries.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+
+import pandas as pd
+
+
+def bicycle_timeseries():
+    return pd.DataFrame()
+
+
+def main():
+    return None
+
+if __name__ == "__main__":
+    main()
+
+# Write function bicycle_timeseries that
+
+   # reads the data set
+  #  cleans it
+ #   turns its Päivämäärä column into (row) DatetimeIndex (that is, to row names) of that DataFrame
+#    returns the new DataFrame
