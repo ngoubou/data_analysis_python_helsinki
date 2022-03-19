3a40b675514f66235394580a892d1ae84acdc20a
Ngoubou <ralphngoubou@gmail.com>
2022-03-19 13:17:57 +0100

    Update ex12_coefficient_of_determination.py
    can't compute the score of the fit, will revert to my code for all predictors

--- week5/ex12_coefficient_of_determination.py
+++ week5/ex12_coefficient_of_determination.py
@@ -9,26 +9,42 @@ os.chdir("/Users/mamba/Downloads/Data_Scientist_Path/Courses/python_helsinki/wee
 
 def coefficient_of_determination():
     df = pd.read_csv("data/mystery_data.tsv", sep = "\t")
-    model = LinearRegression(fit_intercept = True)
-    pred  = []
-    for i in range(len(df)):
-        s = np.array([df.iloc[i, [0, 1, 2, 3, 4]]])
-        pred.append(s)
-    X = np.vstack(pred)
-    Y = np.array(df["Y"])
-    model.fit(X, Y)
-    return [model.score(X, Y)]
+    X = df.loc[:, "X1":"X5"]
+    y = df.Y
+    reg = LinearRegression(fit_intercept = True)
+    
+    m = reg.fit(X, y)
+    
+    #print(df.loc[:, "X1"].shape)
+    x1 = df.loc[:, "X1"]
+    x2 = df.loc[:, "X2"]
+    x3 = df.loc[:, "X3"]
+    x4 = df.loc[:, "X4"]
+    x5 = df.loc[:, "X5"]
+    m1 = reg.fit(x1[:,np.newaxis], y)
+    m2 = reg.fit(x2[:,np.newaxis], y)
+    m3 = reg.fit(x3[:,np.newaxis], y)
+    m4 = reg.fit(x4[:,np.newaxis], y)
+    m5 = reg.fit(x5[:,np.newaxis], y)
+    #ls = [m1, m2, m3, m4, m5]
+    #for i in range(1, 6):
+     #   print("m%d" %i)
+      #  ls.append("m%d" %i)
+    #print(X.shape)
+    #print(X.T.shape)
+    #print(y.shape)
+    
+    #a = reg.score(X, y)
+    #reg.fit
+    #reg.score
+    a = [reg.score(X, y), reg.score(x1[:,np.newaxis], y), reg.score(x2[:,np.newaxis], y),
+    reg.score(x3[:,np.newaxis], y), reg.score(x4[:,np.newaxis], y), reg.score(x5[:,np.newaxis], y)]
+    return a
     
 def main():
     print(coefficient_of_determination())
 if __name__ == "__main__":
     main()
-
-# Part 1.
-
-# Using all the features (X1 to X5), fit the data using a linear regression (include the intercept). 
-# Get the coefficient of determination using the score method of the LinearRegression class. 
-# Write a function coefficient_of_determination to do all this. It should return a list containing the R2-score as the only value.
 
 # Part 2.
 
