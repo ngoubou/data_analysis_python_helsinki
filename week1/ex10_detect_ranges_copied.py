c6cb20393d613c2e38a60f0a505a012fd96aa1f9
Ngoubou <ralphngoubou@gmail.com>
2022-03-22 00:01:18 +0100

    Update ex10_detect_ranges_copied.py
    added course solution to week1 ex10
    did that cause im solving a leetcode and wanted have a starter code

--- week1/ex10_detect_ranges_copied.py
+++ week1/ex10_detect_ranges_copied.py
@@ -3,58 +3,30 @@
  
 
 def detect_ranges(L):
-
     #L.sort()
     ls = sorted(L)
-
     res = []
-
     i=0 
-
     size = len(ls)
 
-    
-
     while i < size-1:
-
         #if next number is +1 bigger
-
         if (ls[i+1]-ls[i] == 1): 
-
             r1= ls[i]        #start of range
-
-            
-
             #loop as long next number is +1 bigger and end not reached
-
             while (i < size-1) and (ls[i+1]-ls[i] == 1):  
-
                 i += 1  
-
             r2 = ls[i]+1        #end of range
-
             res.append(((r1,r2))) #add pair 
-
             i += 1  
-
             if (i==size-1): 
-
                 res.append(ls[i]) #add if next number is last 
-
-        
-
         #if next number was not +1 bigger
-
         else:   
-
             res.append(ls[i]) #add single number
-
             i += 1
-
             if (i==size-1): 
-
                 res.append(ls[i]) #add if next number is last     
-
     return  res
 
  
@@ -62,12 +34,8 @@ def detect_ranges(L):
 def main():
     L = [2, 5, 4, 8, 12, 6, 7, 10, 13]
     #Li = [4, 2, 0, -2, -4]
-
     result = detect_ranges(L)
-
     print(result)
-
- 
 
 if __name__ == "__main__":
 
@@ -76,3 +44,28 @@ if __name__ == "__main__":
 # code is not mine, got it from course discord user @petripa
 # i just modified the "L" list by "ls" in the detect_ranges function
 # prior to that modification, i was failing test 1 and 5.
+
+## Course Solution ----
+
+def detect_ranges(L):
+    if not L:           # empty list
+        return L
+    L = sorted(L)
+#    L.sort()
+    result = []
+    begin, end = L[0], L[0]
+    for x in L:
+        if x == end:
+            end += 1             # increase range
+        else:
+            if begin + 1 == end: # range of length 1
+                result.append(begin)
+            else:
+                result.append((begin, end))
+            begin = x
+            end = begin + 1
+    if begin + 1 == end: # range of length 1
+        result.append(begin)
+    else:
+        result.append((begin, end))
+    return result
