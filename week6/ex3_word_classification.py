c6d89afb5f8894e46a674eefbcc163bd7f61be17
Ngoubou <ralphngoubou@gmail.com>
2022-03-25 13:34:52 +0100

    Update ex3_word_classification.py

--- week6/ex3_word_classification.py
+++ week6/ex3_word_classification.py
@@ -10,30 +10,33 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import cross_val_score
 from sklearn import model_selection
 
-alphabet="abcdefghijklmnopqrstuvwxyzäö-"
+import os
+os.chdir("/Users/mamba/Downloads/Data_Scientist_Path/Courses/python_helsinki/week6")
+
+alphabet = "abcdefghijklmnopqrstuvwxyzäö-"
 alphabet_set = set(alphabet)
 
 # Returns a list of Finnish words
 def load_finnish():
-    finnish_url="https://www.cs.helsinki.fi/u/jttoivon/dap/data/kotus-sanalista_v1/kotus-sanalista_v1.xml"
-    filename="src/kotus-sanalista_v1.xml"
-    load_from_net=False
+    finnish_url = "https://www.cs.helsinki.fi/u/jttoivon/dap/data/kotus-sanalista_v1/kotus-sanalista_v1.xml"
+    filename = "data/kotus-sanalista_v1.xml"
+    load_from_net = False
     if load_from_net:
         with urllib.request.urlopen(finnish_url) as data:
-            lines=[]
+            lines = []
             for line in data:
                 lines.append(line.decode('utf-8'))
-        doc="".join(lines)
+        doc = "".join(lines)
     else:
         with open(filename, "rb") as data:
-            doc=data.read()
+            doc = data.read()
     tree = etree.XML(doc)
     s_elements = tree.xpath('/kotus-sanalista/st/s')
     return list(map(lambda s: s.text, s_elements))
 
 def load_english():
-    with open("src/words", encoding="utf-8") as data:
-        lines=map(lambda s: s.rstrip(), data.readlines())
+    with open("data/words", encoding = "utf-8") as data:
+        lines = map(lambda s: s.rstrip(), data.readlines())
     return lines
 
 def get_features(a):
