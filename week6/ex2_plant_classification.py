e8349d60fd82dd5e256d93f0a5864cb6e416229c
Ngoubou <ralphngoubou@gmail.com>
2022-03-25 13:12:49 +0100

    fit model and compute accuracy

--- week6/ex2_plant_classification.py
+++ week6/ex2_plant_classification.py
@@ -10,8 +10,11 @@ def plant_classification():
     y = load_iris().get("target") 
     y_names = load_iris().get("target_names") # not very useful
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
-    
-    return 0.0
+    model = naive_bayes.GaussianNB()
+    model.fit(X_train, y_train)
+    y_fitted = model.predict(X_test)
+    acc = metrics.accuracy_score(y_test,y_fitted)
+    return acc
 
 def main():
     print(f"Accuracy is {plant_classification()}")
