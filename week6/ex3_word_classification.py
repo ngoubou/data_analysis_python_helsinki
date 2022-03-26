566a8338bc8616f5449d61a25cb97e97a1a611e3
Ngoubou <ralphngoubou@gmail.com>
2022-03-26 21:27:17 +0100

    english words

--- week6/ex3_word_classification.py
+++ week6/ex3_word_classification.py
@@ -111,40 +111,47 @@ finnish = load_finnish()
 #print(len(finnish))
 english = list(load_english())
 #print(len(english))
-
+#s = "As"
+#ss = "Aachen's"
+#print(s.split("'")[0])
+#print(ss.split("'"))
+#print(ss.split("'")[0])
+#print(ss.split("'")[0].isupper())
+#print(ss.split("'")[0].istitle())
+#print(len(s.split("'")))
+#print(len(ss.split("'")))
     # Convert the Finnish words to lowercase, and then filter out those words that contain characters that don't belong to the alphabet.
-#a = np.array(finnish)
 finnish = [x.lower() for x in finnish]
-#f = [x.lower() for x in alphabet]
-#a = {"r", "a", "b"}
-#for i in "bara":
- #   if i in a:
-  #      print("yes")
+
 for k, i in enumerate(finnish):
     i_temp = i
-    #if i == "bébé":
-        #i
     if re.findall(r'\s', i):
-        #i_temp = i
-        #if i_temp == "beauty box":
-         #   i
         i = i.replace(" ", "")
-    #if i == "à la":
-     #   i
     for j in i:
         if j not in alphabet_set:
-            #print(finnish[k])
             if re.findall(r'\s', i_temp):
                 finnish.remove(i_temp)
                 break
             else:
                 finnish.remove(i)
                 break
-            #print(finnish[k]) 
-            #finnish.remove("à la")
-            #i.casefold()
-#print(len(finnish))
     # For the English words first filter out those words that begin with an uppercase letter to get rid of proper nouns. 
+#print(len(english))
+a = english.copy()
+for i in english:
+        #if i == "Aachen's":
+         #   i
+    if i.istitle() or i.isupper():
+        a.remove(i)
+    elif (len(i.split("'")) > 1) and (i.split("'")[0].istitle() or i.split("'")[0].isupper()):
+        a.remove(i)
+english = a
+#print(len(english))
+#print(len(english) - len(a))
+i
+        #elif (re.findall(r"'", i)) :
+         #   a.remove(i)
+            #i
     # Then proceed as with the Finnish words.
 
 # Use get_features function you made earlier to form the feature matrix.
