78aec589deec681839a1daa707367b356bc77007
Ngoubou <ralphngoubou@gmail.com>
2022-03-31 13:38:21 +0200

    done

--- week6/ex5_plant_clustering.py
+++ week6/ex5_plant_clustering.py
@@ -1,19 +1,28 @@
 #!/usr/bin/env python3
 
 import scipy
-
+from sklearn.cluster import KMeans
+from sklearn.datasets import load_iris
+from sklearn.metrics import accuracy_score
 
 def find_permutation(n_clusters, real_labels, labels):
-    permutation=[]
+    permutation = []
     for i in range(n_clusters):
         idx = labels == i
         # Choose the most common label among data points in the cluster
-        new_label=scipy.stats.mode(real_labels[idx])[0][0]
+        new_label = scipy.stats.mode(real_labels[idx])[0][0]
         permutation.append(new_label)
     return permutation
 
 def plant_clustering():
-    return 0.0
+    X = load_iris().get("data")
+    y = load_iris().get("target")
+    model = KMeans(3, random_state = 0)
+    model.fit(X)
+    acc = accuracy_score(y, model.labels_)
+    permutation = find_permutation(3, y, model.labels_)
+    new_labels = [ permutation[label] for label in model.labels_]   # permute the labels
+    return accuracy_score(y, new_labels)
 
 def main():
     print(plant_clustering())
@@ -21,5 +30,18 @@ def main():
 if __name__ == "__main__":
     main()
 
-# Using the same iris data set that you saw earlier in the classification, apply k-means clustering with 3 clusters. 
-# Create a function plant_clustering that loads the iris data set, clusters the data and returns the accuracy_score.
+## Course Solution ----
+# This one was easy cause i just had to follow the course.
+
+#from sklearn.datasets import load_iris
+#from sklearn import cluster
+#import sklearn
+#def plant_clustering():
+    #data = load_iris()
+   # X = data.data
+  #  y = data.target
+    #model = cluster.KMeans(3, random_state=0)
+   # model.fit(X)
+  #  permutation = find_permutation(3, y, model.labels_)
+ #   acc = sklearn.metrics.accuracy_score(y, [ permutation[label] for label in model.labels_])
+#    return acc
