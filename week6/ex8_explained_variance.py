38d525bf81e423175279c9666e1041598e2b447b
Ngoubou <ralphngoubou@gmail.com>
2022-04-01 22:44:26 +0200

    Update ex8_explained_variance.py

--- week6/ex8_explained_variance.py
+++ week6/ex8_explained_variance.py
@@ -20,42 +20,19 @@ def explained_variance():
 
 def main():
     v, ev = explained_variance()
-    
-    #width = 10
-    #precision = 3
     print("The variances are:", end = " ")
-    for i in v:
-        print(f"{i:.3f}", end = " ")
+    [print(f"{v[i]:.3f}", end = " ") for i in range(len(v))]
     print("\n", end = "")
-    print("The variances after PCA are:", end = " ")
-    for j in ev:
-        print(f"{j:.3f}", end = " ")
-    print(f"The variances are:{v}")
-    print(f"The variances after PCA are:{ev}")
+    #print(f"The variances are: {v[0]:.3f} {v[1]:.3f} {v[2]:.3f} {v[3]:.3f} {v[4]:.3f} {v[5]:.3f} {v[6]:.3f} {v[7]:.3f} {v[8]:.3f} {v[9]:.3f}")
+    #print(f"The explained variances after PCA are: {ev[0]:.3f} {ev[1]:.3f} {ev[2]:.3f} {ev[3]:.3f} {ev[4]:.3f} {ev[5]:.3f} {ev[6]:.3f} {ev[7]:.3f} {ev[8]:.3f} {ev[9]:.3f}")
+    print("The explained variances after PCA are:", end = " ")
+    [print(f"{ev[i]:.3f}", end = " ") for i in range(len(ev))]
+
+    #print(f"The variances are: {[v[i]for i in range(len(v))]}")
+    #print(f"The variances after PCA are: {ev[0]:.3f}")
     
     plt.plot(np.arange(1,11), np.cumsum(ev));
     plt.show()
 
 if __name__ == "__main__":
     main()
-
-# This exercise can give two points at maximum!
-
-# Part 1.
-
-# Write function explained_variance which reads the tab separated file "data.tsv". The data contains 10 features. Then fit PCA to the data.
-df = pd.read_csv("data/data_pca.tsv", sep = "\t")
-pca = PCA()
-pca.fit(df)
-
-ls = []
-for i in df:
-    ls.append(variance(df[i]))
-
-# Part 2.
-
-# Plot the cumulative explained variances. The y-axis should be the cumulative sum, and the x-axis the number of terms in the cumulative sum.
-v = pca.explained_variance_
-
-plt.plot(np.arange(1,11), np.cumsum(v));
-plt.show()
