6a7bb417b4f3c3b0a9498af8d967d8e92ed74cf2
Ngoubou <ralphngoubou@gmail.com>
2022-04-01 14:58:09 +0200

    feature variances
    did not understand i had to compute the individual variances of each features before the pca

--- week6/ex8_explained_variance.py
+++ week6/ex8_explained_variance.py
@@ -1,12 +1,19 @@
 #!/usr/bin/env python3
 
+from math import sqrt
+import pandas as pd
+from sklearn.decomposition import PCA
+import os
+import numpy as np
+
+os.chdir("/Users/mamba/Downloads/Data_Scientist_Path/Courses/python_helsinki/week6")
 
 def explained_variance():
     return [], []
 
 def main():
     v, ev = explained_variance()
-    print(sum(v), sum(ev))
+    #print(sum(v), sum(ev))
 
 if __name__ == "__main__":
     main()
@@ -15,7 +22,21 @@ if __name__ == "__main__":
 
 # Part 1.
 
-# Write function explained_variance which reads the tab separated file "data.tsv". The data contains 10 features. Then fit PCA to the data. 
+# Write function explained_variance which reads the tab separated file "data.tsv". The data contains 10 features. Then fit PCA to the data.
+df = pd.read_csv("data/data_pca.tsv", sep = "\t")
+pca = PCA()
+pca.fit(df)
+#print(df.head())
+#print(sum(np.sqrt(pca.singular_values_)))
+numerateur = (pca.singular_values_ - pca.mean_)**2
+#print(numerateur)
+denom = pca.n_samples_ - 1
+#print(sum(numerateur/denom))
+#print(3756.851785624242/399)
+print(pca.explained_variance_)
+#print(np.sum(pca.components_))
+#np.pow
+#print(pca.noise_variance_)
 # The function should return two lists (or 1D arrays). The first list should contain the variances of all the features. 
 # The second list should consist of the explained variances returned by the PCA.
 
