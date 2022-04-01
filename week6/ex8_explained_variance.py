5fedf1b41557e9405ded41360ded4c04c1a06bd3
Ngoubou <ralphngoubou@gmail.com>
2022-04-01 22:49:23 +0200

    done

--- week6/ex8_explained_variance.py
+++ week6/ex8_explained_variance.py
@@ -23,16 +23,32 @@ def main():
     print("The variances are:", end = " ")
     [print(f"{v[i]:.3f}", end = " ") for i in range(len(v))]
     print("\n", end = "")
-    #print(f"The variances are: {v[0]:.3f} {v[1]:.3f} {v[2]:.3f} {v[3]:.3f} {v[4]:.3f} {v[5]:.3f} {v[6]:.3f} {v[7]:.3f} {v[8]:.3f} {v[9]:.3f}")
-    #print(f"The explained variances after PCA are: {ev[0]:.3f} {ev[1]:.3f} {ev[2]:.3f} {ev[3]:.3f} {ev[4]:.3f} {ev[5]:.3f} {ev[6]:.3f} {ev[7]:.3f} {ev[8]:.3f} {ev[9]:.3f}")
     print("The explained variances after PCA are:", end = " ")
     [print(f"{ev[i]:.3f}", end = " ") for i in range(len(ev))]
-
-    #print(f"The variances are: {[v[i]for i in range(len(v))]}")
-    #print(f"The variances after PCA are: {ev[0]:.3f}")
-    
     plt.plot(np.arange(1,11), np.cumsum(ev));
     plt.show()
 
 if __name__ == "__main__":
     main()
+
+## Course Solution ----
+# 1st use (by myself) of list comprehension (!!) so pretty happy about it
+# Turns out that course solution use it too so double bam !
+# Knew should have not used for loop for the variance
+# Should stop relying on loops that much and embrace vectorization (confident it'll come at some point)    
+# LAST EXERCISE BEFORE PROJECT !!! APRIL'S FOOL HAHAHA
+
+#def explained_variance():
+ #   df=pd.read_csv("src/data.tsv", sep="\t")
+  #  variance = df.var(axis=0).values
+   # pca = PCA(10)
+    #pca.fit(df)
+    #return variance, pca.explained_variance_
+
+#def main():
+#    v, ev = explained_variance()
+ #   print(sum(v), sum(ev))
+  #  print("The variances are:", " ".join([f"{x:.3f}" for x in v]))
+   # print("The explained variances after PCA are:", " ".join([f"{x:.3f}" for x in ev]))
+    #plt.plot(np.arange(1,11), np.cumsum(ev));
+    #plt.show()
